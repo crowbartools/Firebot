1ca9f483bc422565d4ce77c13cac264ca3bc5959
jgarver <jgarver@teleflora.com>
2017-01-08 20:18:07 -0600

    UI additions.

--- gui/js/renderer.js
+++ gui/js/renderer.js
@@ -2,19 +2,32 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+// Random BG Image
+function randomBG(){
+    var random = Math.floor(Math.random() * 5) + 1 ;
+    $('body').css('background-image', 'url("./images/bg/'+random+'.jpg")');
+}
+randomBG();
+
 // Toggles the side menu.
-$('.menu-toggle').sidr({
-    name: 'main-menu',
-    source: '#main-menu',
-    renaming: false
-});
+function startMenu(){
+    $('.menu-toggle').sidr({
+        name: 'main-menu',
+        source: '#main-menu',
+        renaming: false
+    });
+}
+startMenu();
 
 // Navigates between various main pages.
-$('.navigation a').click( function(e) {
-    e.preventDefault(); 
-    var nextup = $(this).attr('data');
-    $('.current').slideToggle(500).removeClass('current');
-    $('.'+nextup).slideToggle(500).addClass('current');
-    $.sidr('close','main-menu');
-    return false; 
-} );
+function pageNavigation(){
+    $('.navigation a').click( function(e) {
+        e.preventDefault(); 
+        var nextup = $(this).attr('data');
+        $('.current').slideToggle(500).removeClass('current');
+        $('.'+nextup).slideToggle(500).addClass('current');
+        $.sidr('close','main-menu');
+        return false; 
+    } );
+}
+pageNavigation();
