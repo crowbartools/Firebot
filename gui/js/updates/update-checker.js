62dae4973301e816af41ab8c175a19527a31a9ab
jg <jgarver@teleflora.com>
2017-01-23 16:38:05 -0600

    Ah ha! Fixed the version checker.

--- gui/js/updates/update-checker.js
+++ gui/js/updates/update-checker.js
@@ -1,35 +1,32 @@
 // Requirements
-const {ipcRenderer} = require('electron');
 const request = require('request');
-const versionFetch = require('version');
+const JsonDB = require('node-json-db');
+const appVersion = require('electron').remote.app.getVersion();
 
 // Update Checker
 // This will compare the app version number in package.json to the version number in json at firebottle.tv
 // If they are different then it'll show a "new version available" link.
 function updateChecker(){
-    versionFetch.fetch(function(error,version){
-        if(error){
-            console.log(error);
-        } else {
-            request('http://firebottle.tv/Firebot/update-checker.json', function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var json = JSON.parse(body);
-                    var jsonVersion = json.version;
-                    var versionOverride = json.versionOverride;
-                    
-                    // Check if version matches.
-                    if (version !== jsonVersion || versionOverride === true){
-                        $('.updated-version').fadeIn('fast');
-                    } else {
-                        console.log('On newest version.')
-                    }
+    request('http://firebottle.tv/Firebot/update-checker.json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var version = appVersion;
 
-                    $('title').text('Firebot Interactive || v'+version+' || @firebottletv');
-                } else {
-                    console.log(error);
-                }
-            })
-        }
+            var json = JSON.parse(body);
+            var jsonVersion = json.version;
+            var versionOverride = json.versionOverride;
+            
+            // Check if version matches.
+            if (version !== jsonVersion || versionOverride === true){
+                $('.updated-version a').append(' ('+jsonVersion+')');
+                $('.updated-version').fadeIn('fast');
+            } else {
+                console.log('On newest version.')
+            }
+
+            $('title').text('Firebot Interactive || v'+version+' || @firebottletv');
+        } else {
+            console.log(error);
+        };
     })
 }
 
