e002bdc4404e3daf6ee60dd0ee52537c6276b0c6
Erik Bigler <erikbigler@gmail.com>
2017-06-21 01:59:29 -0700

    refactor listenerservice

--- gui/app/services/listenerService.js
+++ gui/app/services/listenerService.js
@@ -1,6 +1,6 @@
 (function(){
   
- // This contains helps handle listening to events coming from the backend
+ // This helps listening to events coming from the backend
  
  const _ = require('underscore')._;
 
@@ -10,49 +10,132 @@
     var service = {};
     
     var registeredListeners = {
-      soundFiles: {},
-      imageFiles: {}
+      filePath: {},
+      connectionStatus: {},
+      connectionChangeRequest: {}
     }
     
-    service.registerSoundFileListener = function(uuid, callback) {
-      registeredListeners.soundFiles[uuid] = callback;
-    }
-    service.unregisterSoundFileListener = function(uuid) {
-      delete registeredListeners.soundFiles[uuid];
-    }
-    
-    service.registerImageFileListener = function(uuid, callback) {
-      registeredListeners.imageFiles[uuid] = callback;
-    }
-    service.unregisterImageFileListener = function(uuid) {
-      delete registeredListeners.imageFiles[uuid];
+    var ListenerType = {
+      IMAGE_FILE: "imageFile",
+      SOUND_FILE: "soundFile",
+      CONNECTION_STATUS: "connectionStatus",
+      CONNECTION_CHANGE_REQUEST: "connectionChangeRequest"
     }
     
+    service.ListenerType = ListenerType;
+    
+    service.registerListener = function(request, callback) {
+      var uuid = request.uuid;
+      if(uuid == null) {
+        uuid = _.uniqueId();
+      }
+    
+      var listener = {
+        uuid: uuid,
+        type: request.type,
+        callback: callback, // the callback when this listener is triggered
+        runOnce: request.runOnce == true // Means the listener will remove itself after the first time its called
+      }
+      
+      var publishEvent = request.publishEvent == true;
+      
+      switch(listener.type) {
+        case ListenerType.IMAGE_FILE:
+        case ListenerType.SOUND_FILE:
+          registeredListeners.filePath[uuid] = listener;
+          if(publishEvent) {
+            if(listener.type == ListenerType.IMAGE_FILE) {
+              ipcRenderer.send('getImagePath', uuid);
+            }
+            else if(listener.type == ListenerType.SOUND_FILE) {
+              ipcRenderer.send('getSoundPath', uuid);
+            }
+          }
+          break;
+        case ListenerType.CONNECTION_STATUS:
+          registeredListeners.connectionStatus[uuid] = listener;
+          // There isn't really a corresponding event to publish for this
+          break;
+        case ListenerType.CONNECTION_CHANGE_REQUEST:
+          registeredListeners.connectionChangeRequest[uuid] = listener;
+          break;
+      }
+    }
+    
+    service.unregisterListener = function(type, uuid) {
+      switch(type) {
+        case ListenerType.IMAGE_FILE:
+        case ListenerType.SOUND_FILE:
+          delete registeredListeners.filePath[uuid];
+          break;
+        case ListenerType.CONNECTION_STATUS:
+          delete registeredListeners.connectionStatus[uuid];
+          break;
+        case ListenerType.CONNECTION_CHANGE_REQUEST:
+          delete registeredListeners.connectionChangeRequest[uuid];
+          break;
+      }
+    }
+    
+    /**
+    * File path event listeners 
+    */
     ipcRenderer.on('gotSoundFilePath', function (event, data){
-      var uniqueid = data.id;
+        parseFilePathEvent(data);
+    });
+    
+    ipcRenderer.on('gotImageFilePath', function (event, data){
+        parseFilePathEvent(data);
+    });
+      
+    function parseFilePathEvent(data) {
+      var uuid = data.id;
       var filepath = "";
       if(data.path != null) {
         filepath = data.path[0];
       }
       
-      var listener = registeredListeners.soundFiles[uniqueid];
-      if(typeof listener === 'function') {
-        listener(filepath);
-      }
+      var listener = registeredListeners.filePath[uuid];
+      runListener(listener, filepath);    
+    }
+    
+    /**
+    * Connection event listeners 
+    */
+    
+    // Connection Monitor
+    // Recieves event from main process that connection has been established or disconnected.
+    ipcRenderer.on('connection', function (event, data) {
+      var isConnected = data ? (data.toLowerCase() == "Online") : false;
+      
+      _.forEach(registeredListeners.connectionStatus, (listener, key, list) => {
+        runListener(listener, isConnected);
+      });
     });
     
-    ipcRenderer.on('gotImageFilePath', function (event, data){
-        var uniqueid = data.id;
-        var filepath = "";
-        if(data.path != null) {
-          filepath = data.path[0];
-        }
-        
-        var listener = registeredListeners.imageFiles[uniqueid];
-        if(typeof listener === 'function') {
-          listener(filepath);
-        }
+    // Connect Request
+    // Recieves an event from the main process when the global hotkey is hit for connecting.
+    ipcRenderer.on('getRefreshToken', function (event, data) {  
+      _.forEach(registeredListeners.connectionChangeRequest, (listener, key, list) => {
+        runListener(listener, null);
       });
+    });
+    
+    /**
+    *  Helpers
+    */
+        
+    function runListener(listener, returnPayload) {
+      if(listener != null) {
+        var callback = listener.callback;
+        if(typeof callback === 'function') {
+          callback(returnPayload);
+        }
+        if(listener.runOnce == true) {
+          service.unregisterListener(listener.type, listener.uuid);
+        }
+      } 
+    }
     
     return service;
   });
