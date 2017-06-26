12d2ffb9b07423103227a687c8e972030f40792b
jgarver <jgarver@teleflora.com>
2017-06-26 12:08:03 -0500

    Updates page

--- gui/app/controllers/updatesController.js
+++ gui/app/controllers/updatesController.js
@@ -4,7 +4,20 @@
  
  angular
    .module('firebotApp')
-   .controller('updatesController', function($scope) {
+   .controller('updatesController', function($sce, $scope, updatesService) {
+      $scope.updates = {};
 
+      // Get update information
+      updatesService.updateCheck()
+        .then(update => {
+          $scope.updates = update;
+
+          // This is required by angular to strip out bad HTML that could cause issues.
+          $scope.updates.gitNotes = $sce.trustAsHtml($scope.updates.gitNotes)
+
+          // Need this as apparently angular doesn't handle promises well and you have to
+          // manually force it to update after variables change.
+          $scope.$apply();
+        });
    });
  })();
