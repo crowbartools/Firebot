aedb82f8803bcf68f41652c056eec0f2fb2ee0b2
ebiggz <erikbigler@gmail.com>
2017-06-28 14:08:48 -0700

    Remove gobal.js from new front end, move its vars to index.js

--- gui/app/index.js
+++ gui/app/index.js
@@ -2,6 +2,17 @@ const electron = require('electron');
 const {ipcRenderer} = electron;
 const {remote} = electron;
 
+//from old Gobal.js
+const shell = require('electron').shell;
+const fs = require('fs');
+const JsonDB = require('node-json-db');
+const request = require('request');
+const List = require('list.js');
+const howler = require('howler');
+const compareVersions = require('compare-versions');
+const marked = require('marked');
+const path = require('path');
+
 require('angular');
 require('angular-animate');
 require('angular-route');
