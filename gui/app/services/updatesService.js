9fe9b9d64946d115b6891372517c7c3f35bc74e5
Erik Bigler <erikbigler@gmail.com>
2017-06-29 08:59:58 -0700

    logic for downloading zips from github, handles redirection

--- gui/app/services/updatesService.js
+++ gui/app/services/updatesService.js
@@ -5,10 +5,13 @@
  const _ = require('underscore')._;
  const JsonDb = require('node-json-db');
  const compareVersions = require('compare-versions');
+ const https = require('https');
+ const urlParser  = require('url');
+
 
  angular
   .module('firebotApp')
-  .factory('updatesService', function ($q, $http, $sce, settingsService) {
+  .factory('updatesService', function ($q, $http, $sce, settingsService, utilityService) {
     // factory/service object
     var service = {}
     
@@ -17,12 +20,9 @@
     service.hasCheckedForUpdates = false;  
     
     service.updateIsAvailable = function() {
-      if(service.hasCheckedForUpdates) {
-        return service.updateData.updateIsAvailable;
-      } else {
-        return false;
-      }
+      return service.hasCheckedForUpdates ? updateData.updateIsAvailable : false;
     }
+  
 
     // Update Checker
     // This checks for updates.
@@ -31,6 +31,9 @@
             // If user opts into betas we want to check different git api links.
             // If they are, we check releases as beta releases will be listed.
             // Else we check /latest which will only list the latest full release.
+            
+            // Start checking
+            autoUpdate.fire('check');
 
             var firebotReleasesUrl = "https://api.github.com/repos/Firebottle/Firebot/releases/latest";
             
@@ -54,12 +57,13 @@
                   gitNewest = response.data[0];
               } else {
                   gitNewest = response.data;
-              }            
+              }          
 
               var gitName = gitNewest.name;
               var gitDate = gitNewest.created_at;
               var gitLink = gitNewest.html_url;
               var gitNotes = marked(gitNewest.body);
+              var gitZipDownloadUrl = gitNewest.assets[0].browser_download_url;
 
               // Now lets look to see if there is a newer version.
               var versionCompare = compareVersions(gitNewest.tag_name, currentVersion);
@@ -75,6 +79,7 @@
                   gitDate: gitDate,
                   gitLink: gitLink,
                   gitNotes: $sce.trustAsHtml(gitNotes),
+                  gitZipDownloadUrl: gitZipDownloadUrl
                   updateIsAvailable: updateIsAvailable
               }
               
@@ -89,6 +94,94 @@
             });
         });
     }
+    
+    
+    service.downloadUpdate = function () {
+      downloadFile(updateData.gitZipDownloadUrl);  
+    }
+    
+    function downloadFile(zipUrl) {
+      var fileName = "firebotUpdate.zip"
+          
+      var parsedUrlOptions = urlParser.parse(zipUrl);
+      
+      parsedUrlOptions.headers = {
+        'Accept': "application/octet-stream"
+      }
+    
+      var request = https.get(parsedUrlOptions, function(res) {
+        // we need to redirect
+        if(res.statusCode === 302) {
+          console.log("Redirecting...")
+          service.downloadUpdate(res.headers.location);
+          return;
+        }
+        
+        // Check if the file already exists and remove it if it does
+        if (fs.existsSync('_' + fileName)) fs.unlinkSync('_' + fileName);
+    
+        // Download started
+        //emit(self, 'download.start', name);
+        console.log("Download started!");
+    
+        // Writestream for the binary file
+        var file = fs.createWriteStream('_' + fileName),
+          len = parseInt(res.headers['content-length'], 10),
+          current = 0;
+    
+        // Pipe any new block to the stream
+        res.pipe(file);
+    
+        var dataRecieve = function(chunk) {
+          current += chunk.length;
+          perc = (100.0 * (current / len)).toFixed(2);
+          //emit(self, 'download.progress', name, perc);
+          console.log(`Downloaded: ${perc}%`);
+        };
+      
+    
+        res.on('data', _.debounce(dataRecieve, 100));
+
+    
+        res.on('end', function() {
+          file.end();
+        });
+    
+        file.on('finish', function() {
+          fs.renameSync('_' + fileName, fileName);
+          //emit(self, 'download.end', name);
+          console.log("Download finished!!");
+    
+          //deferred.resolve();
+        });
+      });
+      request.end();
+      request.on('error', function(e) {
+        //deferred.reject();
+        //emit(self, 'download.error', e);
+        console.log("Error", e);
+      });
+    }
+    
+    /* Extract logic.
+    var extract = function(name, subfolder) {
+      var admzip = require('adm-zip');
+    
+      var zip = new admzip(name);
+      var zipEntries = zip.getEntries(); // an array of ZipEntry records
+      var deferred = Defer();
+    
+      if (subfolder) {
+        zip.extractAllTo('./', true);
+      } else {
+        zip.extractEntryTo(zipEntries[0], './', false, true);
+      }
+    
+      fs.unlink(name, deferred.resolve.bind(deferred));
+      return deferred;
+    };
+    */
+
     return service;
   });
 })();
