3afa6ef315855f3ba4e5142a58f5af6a3f12e396
Firebottle <firebottle@firebottle.tv>
2017-07-08 01:40:41 -0500

    Unhandled exception logging?

--- main.js
+++ main.js
@@ -145,7 +145,8 @@ function pathExists(path) {
 
   process.on('uncaughtException', function(error) {
       // Handle the error
-      // console.error(error);
+      renderWindow.webContents.send('error', "There was an unhandled error. Please see /user-settings/logs for more details.");
+      errorLog(error);
   });
 
   // When Quittin.
@@ -159,3 +160,23 @@ function pathExists(path) {
 
 // Interactive handler
 const mixerConnect = require('./lib/interactive/mixer-interactive.js');
+
+// Errors
+function errorLog(error){
+  var date = new Date();
+  var fileName = (date.getMonth()+1)+'-'+date.getDate()+'-'+date.getFullYear()+'-'+date.getHours()+'-'+date.getMinutes()+'-'+date.getSeconds()
+  fs.readFile('./user-settings/controls/'+lastBoard+'.json', (err, data) => {
+    if(!err){
+      var lastBoard = data;
+    } else {
+      var lastBoard = "Error getting last board json.";
+    }
+    var stream = fs.createWriteStream("./user-settings/logs/"+fileName+".log");
+      stream.once('open', function(fd) {
+      stream.write(error+'\r\n');
+      stream.end();
+    });
+
+    console.log('Wrote error to file.')
+  });
+}
