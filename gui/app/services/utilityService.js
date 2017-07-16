745f1b03e24ec0c0bf4e81f5b811a742bc9bd6d4
Firebottle <firebottle@firebottle.tv>
2017-07-16 13:02:22 -0500

    Fixing utility service.

--- gui/app/services/utilityService.js
+++ gui/app/services/utilityService.js
@@ -2,14 +2,14 @@
   
  // This contains utility functions
  // Just inject "utilityService" into any controller that you want access to these
- 
-  const _ = require('underscore')._;
-  const fs = require('fs')
-  const logger = require('../../lib/errorLogging.js');
+ const electron = require('electron');
+ const _ = require('underscore')._;
+ const path = require('path');
+ const logger = require('../../lib/errorLogging.js');
 
  angular
   .module('firebotApp')
-  .factory('utilityService', function ($rootScope, $uibModal, listenerService, settingsService) {
+  .factory('utilityService', function ($rootScope, $uibModal, listenerService) {
     var service = {};
       
     service.showModal = function(showModalContext) {
@@ -59,7 +59,6 @@
         templateUrl: "./templates/misc-modals/firstTimeUseModal.html",
         // This is the controller to be used for the modal. 
         controllerFunc: "firstTimeUseModalController",
-        // The callback to run after the modal closed via "Add board"
         keyboard: false,
         backdrop: 'static',
         closeCallback: (data) => {
@@ -67,6 +66,54 @@
         }
       }      
       service.showModal(firstTimeUseModalContext);
+    };
+    
+    /*
+    * OVERLAY INFO MODAL
+    */
+    service.showOverlayInfoModal = function() {
+      var overlayInfoModalContext = {
+        templateUrl: "overlayInfoModal.html",
+        // This is the controller to be used for the modal. 
+        controllerFunc: ($scope, $rootScope, $uibModalInstance) => {        
+          
+          $scope.overlayPath = 
+            path.resolve(process.cwd() + path.sep + "overlay" + path.sep + "firebot.html");
+            
+          $scope.pathCopied = false;
+            
+          $scope.copy = function() {
+            $rootScope.copyTextToClipboard($scope.overlayPath);
+            $scope.pathCopied = true;
+          }
+          
+          $scope.dismiss = function() {
+            $uibModalInstance.dismiss('cancel');
+          };
+        }
+      }      
+      service.showModal(overlayInfoModalContext);
+    };
+    /*
+    * JUST UPDATED MODAL
+    */
+    service.showUpdatedModal = function() {
+      var justUpdatedModalContext = {
+        templateUrl: "updatedModal.html",
+        // This is the controller to be used for the modal. 
+        controllerFunc: ($scope, $uibModalInstance) => {
+          
+          var appVersion = electron.remote.app.getVersion();
+          
+          $scope.appVersion = `v${appVersion}`;
+          
+          $scope.dismiss = function() {
+            $uibModalInstance.dismiss('cancel');
+          };
+        },
+        size: "sm"
+      }      
+      service.showModal(justUpdatedModalContext);
     };
     
     service.showErrorModal = function (errorMessage) {
