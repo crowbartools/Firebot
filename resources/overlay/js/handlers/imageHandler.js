6e2428d78545959fc07aa6da7126616b11b973ba
Erik Bigler <erikbigler@gmail.com>
2017-08-08 18:16:06 -0700

    image position fixes

--- resources/overlay/js/handlers/imageHandler.js
+++ resources/overlay/js/handlers/imageHandler.js
@@ -8,22 +8,22 @@ function showImage(data){
 	var imagePosition = data.imagePosition;
 	var imageHeight = data.imageHeight;
 	var imageWidth = data.imageWidth;
-	console.log(data.imageHeight);
+
 	var imageDuration = parseInt(data.imageDuration) * 1000;
 
 	// Get time in milliseconds to use as class name.
 	var d = new Date();
 	var divClass = d.getTime();
 
-	if (imageHeight === false && imageWidth === false){
+	if (imageHeight == false && imageWidth == false){
 		// Both height and width fields left blank.
 		var imageFinal = '<div class="'+divClass+'-image imageOverlay" position="'+imagePosition+'" style="display:block;"><img src="'+filepathNew+'?time='+divClass+'" position="'+imagePosition+'"></div>';
-	} else if (imageWidth === false){
+	} else if (imageWidth == false){
 		// Width field left blank, but height provided.
 		var imageFinal = '<div class="'+divClass+'-image imageOverlay" position="'+imagePosition+'" style="display:block;height:'+ imageHeight +'px;"><img src="'+filepathNew+'?time='+divClass+'" position="'+imagePosition+'" style="max-width:100%; max-height:100%;height:'+ imageHeight +'px;"></div>';
-	} else if (imageHeight === false) {
+	} else if (imageHeight == false) {
 		// Height field left blank, but width provided.
-		var imageFinal = '<div class="'+divClass+'-image imageOverlay" position="'+imagePosition+'" style="display:block;width:'+ imageWidth +'px;"><img src="'+filepathNew+'?time='+divClass+'" position="'+imagePosition+'" style="max-width:100%; max-height:100%;width:'+ imageWidth +'px;"></div>';
+		var imageFinal = '<div class="'+divClass+'-image imageOverlay" style="width:100%;height:100%;position:relative;"><img src="'+filepathNew+'?time='+divClass+'" position="'+imagePosition+'" style="max-width:100%; max-height:100%;width:'+ imageWidth +'px;"></div>';
 	} else {
 		// Both height and width provided.
 		var imageFinal = '<div class="'+divClass+'-image imageOverlay" position="'+imagePosition+'" style="display:block;height:'+ imageHeight +'px; width:'+ imageWidth +'px;"><img src="'+filepathNew+'?time='+divClass+'" position="'+imagePosition+'" style="max-width:100%; max-height:100%;"></div>';
