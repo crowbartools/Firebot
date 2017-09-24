fb948f48ca877cf581cdf860fbcaf63e637a03b5
Firebottle <firebottle@firebottle.tv>
2017-09-24 02:03:59 -0500

    some comments

--- gui/app/controllers/commandsController.js
+++ gui/app/controllers/commandsController.js
@@ -5,16 +5,18 @@
  angular
    .module('firebotApp')
    .controller('commandsController', function($scope, commandsService, updatesService, utilityService, settingsService) {
-     // Refresh commands cache on load
+     // Cache commands on app load.
      commandsService.refreshCommands();
 
      // TO DO: Commands needs it's own button view.
      $scope.buttonViewMode = settingsService.getButtonViewMode();
 
+     // Gets an array of command types.
      $scope.getCommandTypes = function(){
          return commandsService.getCommandTypes();
      }
 
+     // Gets all commands within a certain command type.
      $scope.getAllCommandsForType = function(commandType){
          return commandsService.getAllCommandsForType(commandType);
      }
