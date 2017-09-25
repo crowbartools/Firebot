8ad3771c5b43f68e41d37f84ff107d255ecc8c65
jgarver <jgarver@teleflora.com>
2017-09-25 11:36:46 -0500

    Add commands, modify effects, copy/paste/delete effects.

--- gui/app/controllers/commandsController.js
+++ gui/app/controllers/commandsController.js
@@ -1,6 +1,8 @@
 (function(){
   
  //This handles the commands tab
+ const _ = require('underscore')._;
+ const EffectType = require('../../lib/interactive/EffectType.js').EffectType;
  
  angular
    .module('firebotApp')
@@ -28,13 +30,142 @@
 
     // This opens up a modal when adding a new command.
     $scope.showAddCommandModal = function(){
-      console.log('Success!');
+      var addCommandModalContext = {
+        templateUrl: "./templates/chat/command-modals/addCommandModal.html",
+        // This is the controller to be used for the modal.
+        controllerFunc: ($scope, $uibModalInstance) => {
+          // This sets the command to active. We're assuming all new commands will be active right away.
+          $scope.command = {active: true};
+
+          // Grab the EffectType 'enum' from effect.js
+          $scope.effectTypes = EffectType;
+
+          // This makes sure the last effect is open upon modal load.
+          // We also call this when a new effect is added or an old effect is deleted
+          // to open the last effect again.
+          $scope.openEffectPanel = {}
+          function updateOpenPanel() {
+            // We get the index of the last effect and add true to a scope varible
+            // that the accordian directive is looking at
+            var lastEffectIndex = _.keys($scope.command.effects).length - 1;
+            $scope.openEffectPanel[lastEffectIndex] = true;
+          }
+
+          $scope.getApprovedEffectTypes = function() {
+            // Convert effecttypes to an array
+            var approvedEffects =  Object.keys(EffectType).map(function(key) {
+                  return EffectType[key];
+                });
+            if(!settingsService.getCustomScriptsEnabled()) {
+              // If there are certain effect types that are available contionally,
+              // we can filter them out here. Currently we only need this for the
+              // Custom Script effect type.
+              approvedEffects = approvedEffects.filter(type => {
+                return type !== EffectType.CUSTOM_SCRIPT;
+              });
+            }
+            return approvedEffects;
+          }
+
+          // When the user clicks "Save"
+          $scope.saveChanges = function() {
+            $uibModalInstance.close($scope.command);
+          };
+
+          $scope.changeEffectTypeForEffect = function(effectType, effect) {
+            for (var property in effect){
+              if (effect.hasOwnProperty(property)){
+                  delete effect[property];
+              }
+            }
+            effect.type = effectType;
+          }
+
+          // When they hit cancel or click outside the modal, we dont want to do anything
+          $scope.dismiss = function() {
+            $uibModalInstance.dismiss('cancel');
+          };
+
+          $scope.addEffect = function() {
+            var newEffectIndex = 1;
+
+            if($scope.command.effects != null) {
+              newEffectIndex = _.keys($scope.command.effects).length + 1;
+            } else {
+              // Make sure effects object is initialized
+              $scope.command.effects = {};
+            }
+
+            $scope.command.effects[newEffectIndex.toString()] = {
+              type: "Nothing"
+            };
+
+            updateOpenPanel();
+          }
+
+          $scope.removeEffectAtIndex = function(index) {
+            //set the previous open panel to false so whatever gets moved to the previous
+            //slot doesnt auto-open
+            $scope.openEffectPanel[index] = false;
+
+            // remove effect
+            delete $scope.command.effects[(index+1).toString()];
+
+            //recalculate index numbers
+            var newEffects = {};
+            var count = 1;
+            Object.keys($scope.command.effects).forEach(key => {
+              var effect = $scope.command.effects[key];
+              newEffects[count.toString()] = effect;
+              count++;
+            });
+
+            $scope.command.effects = newEffects;
+          }
+
+          $scope.removeAllEffects = function() {
+            $scope.command.effects = {};
+          };
+
+          $scope.copyEffects = function() {
+              utilityService.copyButtonEffects($scope.command.effects);
+          };
+
+          $scope.pasteEffects = function() {
+              if(utilityService.hasCopiedEffects()) {
+                $scope.command.effects = utilityService.getCopiedButtonEffects();
+              }
+          };
+
+          $scope.hasCopiedEffects = function() {
+            return utilityService.hasCopiedEffects();
+          };
+
+        },
+        // The callback to run after the modal closed via "Save changes"
+        closeCallback: (command) => {
+
+          // Save to json
+          commandsService.saveCommand(command);
+
+          // Refresh cache
+          commandsService.refreshCommands();
+
+        }
+      }
+      utilityService.showModal(addCommandModalContext);
     }
 
     // This opens a modal when editing a command.
     $scope.showEditCommandEffectsModal = function(){
       console.log('Success!');
     }
+
+    ///////////
+    // Effects (Can we put these somewhere they can be shared between interactive and commands?)
+    ///////////
+
+
     
    });
  })();
--- gui/app/services/commandsService.js
+++ gui/app/services/commandsService.js
@@ -31,7 +31,41 @@
 
         // Return all commands for a specific command type.
         service.getAllCommandsForType = function(commandType){
-            return commandsCache[commandType];
+            var commandArray = [];
+            if (commandsCache != null) {
+                var commands = commandsCache[commandType];
+                for (command in commands){
+                    var command = commands[command];
+                    commandArray.push(command);
+                }
+            }
+            return commandArray;
+        }
+
+        // Saves out a command
+        service.saveCommand = function(command) {
+            var commandDb = dataAccess.getJsonDbInUserData("/user-settings/chat/commands");
+            
+            // Note(ebiggz): Angular sometimes adds properties to objects for the purposes of two way bindings
+            // and other magical things. Angular has a .toJson() convienence method that coverts an object to a json string
+            // while removing internal angular properties. We then convert this string back to an object with 
+            // JSON.parse. It's kinda hacky, but it's an easy way to ensure we arn't accidentally saving anything extra.
+            var cleanedCommands = JSON.parse(angular.toJson(command));
+            
+            // If the command is active, throw it into the active group. Otherwise put it in the inactive group.
+            if(command.active === true){
+                console.log('Saving '+command.commandID+' to active');
+                try{
+                    commandDb.delete("/Inactive/" + command.commandID);
+                }catch(err){}
+                commandDb.push("/Active/" + command.commandID, cleanedCommands);
+            } else {
+                console.log('Saving '+command.commandID+' to inactive');
+                try{
+                    commandDb.delete("/Active/" + command.commandID);
+                }catch(err){}
+                commandDb.push("/Inactive/" + command.commandID, cleanedCommands);
+            }
         }
 
         return service;
