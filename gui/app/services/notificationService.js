26870ed216831404a60b229a528988a3f48ccb2a
Erik Bigler <erikbigler@gmail.com>
2017-10-02 16:34:29 -0700

    notificationservice tweaks

--- gui/app/services/notificationService.js
+++ gui/app/services/notificationService.js
@@ -8,11 +8,21 @@
   .module('firebotApp')
   .factory('notificationService', function ($http) {
 
-    function getNotificationsFile() {
-      return dataAccess.getJsonDbInUserData("/user-settings/notifications");
+    var service = {};
+
+    const NotificationType = {
+      EXTERNAL: "external",
+      UPDATE: "update",
+      MISC: "misc"
     }
 
-    var service = {};
+    const NotificationIconType = {
+      UPDATE: "update",
+      INFO: "info",
+      TIP: "tip",
+      WARNING: "warning"
+    }
+
 
     /*
     example notification:
@@ -29,14 +39,33 @@
     
     var notifications = [];
 
-
-
-
     service.loadExternalNotifications = function() {
       $http.get("https://raw.githubusercontent.com/Firebottle/Firebot/dev/resources/notifications.json").then((response) => {
+        var externalNotifications = response.data;
 
+        var knownExtNotis = getKnownExternalNotifications();
 
-        console.log(response.data);
+        var newKnownExtNotis = [];
+
+        externalNotifications.forEach((n) => {
+
+          var previouslyKnownNoti = findKnownExternalNoti(n.id, knownExtNotis);
+          newKnownExtNotis.push({id: n.id, deleted: previouslyKnownNoti.deleted == true});
+
+          if(previouslyKnownNoti == null) {
+                       
+            service.addNotification({
+                type: NotificationType.EXTERNAL,
+                icon: n.icon ? n.icon : NotificationIconType.INFO,
+                title: n.title,
+                message: n.message
+              }, 
+              true);
+
+          }
+        });
+
+        setKnownExternalNotifications(newKnownExtNotis);
       });
     }
 
@@ -45,9 +74,43 @@
     }
 
     service.addNotification = function(notification, permenantlySave = false) {
-
+      notification.uuid = uuid();
+      notification.timestamp = new Date();
+      notification.read = false;
+      notification.deleted = false;
+      notifications.push(notification);
     }
 
+    /* Helpers */
+
+    function getNotificationsFile() {
+      return dataAccess.getJsonDbInUserData("/user-settings/notifications");
+    }
+
+    function getKnownExternalNotifications() {
+      var externalNotiIds = getNotificationsFile().getData("/knownExternalNotifications")
+      return externalNotiIds ? externalNotiIds : [];
+    }
+
+    function getKnownExternalNotifications() {
+      var externalNotiIds = getNotificationsFile().getData("/knownExternalNotifications")
+      return externalNotiIds ? externalNotiIds : [];
+    }
+
+    function setKnownExternalNotifications(notis) {
+      getNotificationsFile().push("/knownExternalNotifications", notis);
+    }
+
+    function findKnownExternalNoti(id, knownExternalNotis) {
+      var known = knownExternalNotis.filter((n) => n.id == id);
+      return known;
+    }
+
+    function uuid() {
+      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+      )
+    }
     
     return service;
   });
