5d8ad65626ab424331f194ef3f79cc6defc7de09
Erik Bigler <erikbigler@gmail.com>
2017-12-13 16:30:35 -0800

    make effect type list order alphabetically

--- gui/app/directives/controls/searchableEffectDropdown.js
+++ gui/app/directives/controls/searchableEffectDropdown.js
@@ -15,7 +15,7 @@
             template: `
       <ui-select ng-model="$ctrl.selectedEffect" on-select="$ctrl.selectOption($item, $model)" theme="bootstrap">
         <ui-select-match placeholder="Select or search for an effect... ">{{$select.selected.name}}</ui-select-match>
-        <ui-select-choices repeat="option in $ctrl.options | filter: { name: $select.search }" style="position:relative;">
+        <ui-select-choices repeat="option in $ctrl.options | filter: { name: $select.search } | orderBy:'toString()'" style="position:relative;">
           <div ng-bind-html="option.name | highlight: $select.search"></div>
           <small class="muted">{{option.description}}</small>
 
