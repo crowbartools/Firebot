c5bc90fe723fcab2dc7b9c4e93e8648840697123
Erik Bigler <erikbigler@gmail.com>
2018-01-23 02:07:25 -0800

    fix bug when getting null board name

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
-
+    
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         'create-windows-installer': {
             x64: {
-                appDirectory: 'G:/GitHub/Firebot/dist/Firebot-win32-x64',
-                outputDirectory: 'G:/GitHub/Firebot/dist/installer64',
-                loadingGif: 'G:/GitHub/Firebot/gui/images/animated.gif',
+                appDirectory: 'C:/Users/footb/Documents/GitHub/Firebot/dist/Firebot-win32-x64',
+                outputDirectory: 'C:/Users/footb/Documents/GitHub/Firebot/dist/installer64',
+                loadingGif: 'C:/Users/footb/Documents/GitHub/Firebot/gui/images/animated.gif',
                 iconUrl: 'https://firebottle.tv/Firebot/installer/logo.ico',
-                setupIcon: 'G:/GitHub/Firebot/gui/images/logo.ico'
+                setupIcon: 'C:/Users/footb/Documents/GitHub/Firebot/gui/images/logo.ico'
             }
         }
     });
