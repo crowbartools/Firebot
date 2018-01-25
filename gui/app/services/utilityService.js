6676ef7158640439996c6dfa03181ed245c513fa
Erik Bigler <erikbigler@gmail.com>
2018-01-25 02:28:05 -0800

    add generate uuid util function

--- gui/app/services/utilityService.js
+++ gui/app/services/utilityService.js
@@ -583,6 +583,13 @@
                 return first.toUpperCase() + rest.join('').toLowerCase();
             };
 
+            service.generateUuid = function() {
+                // RFC4122 version 4 compliant
+                return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+                    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+                );
+            };
+
             return service;
         });
 }());
