35daef07406addb9fc10cfc4eb4f98e7f17911d7
Erik Bigler <erikbigler@gmail.com>
2018-02-02 00:50:21 -0800

    update logger used in utilityService

--- gui/app/services/utilityService.js
+++ gui/app/services/utilityService.js
@@ -4,13 +4,12 @@
     // This contains utility functions
     // Just inject "utilityService" into any controller that you want access to these
     const electron = require('electron');
-    const logger = require('../../lib/errorLogging.js');
 
     const _ = require('underscore')._;
 
     angular
         .module('firebotApp')
-        .factory('utilityService', function($rootScope, $uibModal, listenerService) {
+        .factory('utilityService', function($rootScope, $uibModal, listenerService, logger) {
             let service = {};
 
             let copiedEffectsCache = {};
@@ -289,7 +288,7 @@
                 service.showModal(errorModalContext);
 
                 // Log error to file.
-                logger.log(errorMessage);
+                logger.error(errorMessage);
             };
 
             /*
@@ -417,7 +416,7 @@
                 service.showModal(infoModalContext);
 
                 // Log info to file.
-                logger.log(infoMessage);
+                logger.info(infoMessage);
             };
 
             /*
