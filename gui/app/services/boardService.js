4bc223aed198d1c9e9566e888b88174fd88f6e56
Firebottle <firebottle@firebottle.tv>
2018-02-03 16:07:34 -0600

    lintman

--- gui/app/services/boardService.js
+++ gui/app/services/boardService.js
@@ -6,7 +6,6 @@
     const fs = require('fs');
     const _ = require('underscore')._;
     const dataAccess = require('../../lib/common/data-access.js');
-    const logger = require('../../lib/errorLogging.js');
 
     angular
         .module('firebotApp')
