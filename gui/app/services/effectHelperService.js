4247bd0b6c29d7ec853298f06215a9c9d7655a95
Erik Bigler <erikbigler@gmail.com>
2018-05-31 00:53:09 -0700

    allow textboxes to be updated in the Update Button effect

--- gui/app/services/effectHelperService.js
+++ gui/app/services/effectHelperService.js
@@ -348,11 +348,11 @@
                         }
 
                         $scope.buttons = boardService.getControlsForSelectedBoard()
-                            .filter(c => c.kind === "button")
+                            .filter(c => c.kind === "button" || c.kind === "textbox")
                             .map(b => {
                                 return {
                                     controlId: b.controlId,
-                                    text: b.text,
+                                    text: b.text != null && b.text.trim() !== "" ? b.text : b.controlId,
                                     scene: b.scene
                                 };
                             });
