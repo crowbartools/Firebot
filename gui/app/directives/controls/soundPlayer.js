b361564ae4af3e6616ed1bdda78aec19f173a5d7
Erik Bigler <erikbigler@gmail.com>
2018-10-26 12:23:41 -0700

    implement sound player for Play Sound effect

--- gui/app/directives/controls/soundPlayer.js
+++ gui/app/directives/controls/soundPlayer.js
@@ -7,14 +7,23 @@
         .component("soundPlayer", {
             bindings: {
                 path: "<",
-                volume: "<"
+                volume: "<",
+                outputDevice: "<"
             },
             template: `
-            <div style="display: flex;flex-direction: row;align-items: center;">
-
+            <div ng-class="{'disabled-control': !$ctrl.path }" class="sound-player">
+                <div ng-click="playOrPause()" ng-class="{'disabled-control': !$ctrl.path,  'clickable': $ctrl.path}" style="padding-right: 10px;">
+                    <i class="fas" ng-class="{'fa-play': !isPlaying(), 'fa-pause': isPlaying()}"></i>
+                </div>
+                <div ng-click="stop()" ng-class="{'disabled-control': !$ctrl.path,  'clickable': $ctrl.path}" style="padding-right: 10px;">
+                    <i class="fas fa-stop"></i>
+                </div>
+                <span>
+                 {{seekPositionDisplay}} / {{durationDisplay}} 
+                </span>
             </div>
             `,
-            controller: function($scope, soundService) {
+            controller: function($scope, $interval, soundService) {
                 let $ctrl = this;
 
                 $scope.seekPositionDisplay = "0:00";
@@ -23,34 +32,62 @@
 
                 let sound = null;
 
-                let seekPositionTimer = null;
+                function pad(num) {
+                    let s = num + "";
+                    while (s.length < 2) s = "0" + s;
+                    return s;
+                }
 
                 function getDurationDisplay(duration) {
                     let totalSecs = Math.round(duration);
 
                     let display = "";
                     if (totalSecs < 60) {
-                        display = `0:${totalSecs}`;
+                        display = `0:${pad(totalSecs)}`;
                     } else {
                         let totalMins = totalSecs / 60;
                         let remainingSecs = totalSecs % 60;
-                        display = `${totalMins}:${remainingSecs}`;
+
+                        display = `${totalMins}:${pad(remainingSecs)}`;
                     }
                     return display;
                 }
 
+                let previousSeek = 0;
+                let seekPositionTimer = $interval(() => {
+                    if (sound != null) {
+                        let currentSeek = sound.seek();
+                        if (currentSeek !== previousSeek) {
+                            $scope.seekPositionDisplay = getDurationDisplay(currentSeek);
+                            previousSeek = currentSeek;
+                        }
+                    }
+                }, 250);
+
+
                 function loadSound() {
-                    if ($ctrl.path != null || $ctrl.path.length === 0) {
+                    if (sound != null) {
+                        sound.unload();
+                        sound = null;
+                    }
+                    if ($ctrl.path == null || $ctrl.path.length === 0) {
                         $scope.seekPositionDisplay = "0:00";
                         $scope.durationDisplay = "0:00";
                         $scope.controlsEnabled = false;
                         return;
                     }
-                    let volume = 5;
+
+                    let volume = 0.5;
                     if (!isNaN($ctrl.volume)) {
                         volume = $ctrl.volume;
+                        if (volume > 10) {
+                            volume = 1;
+                        } else if (volume > 0) {
+                            volume = volume / 10;
+                        }
                     }
-                    soundService.getHowlSound($ctrl.path, volume)
+
+                    soundService.getHowlSound($ctrl.path, volume, $ctrl.outputDevice)
                         .then(s => {
                             sound = s;
 
@@ -60,21 +97,61 @@
                             });
 
                             sound.load();
-
                         });
                 }
 
+                $scope.isPlaying = () => {
+                    if (sound == null) return false;
+                    return sound.playing();
+                };
+
+                $scope.playOrPause = () => {
+                    if (sound == null || !$scope.controlsEnabled) return;
+                    if (sound.playing()) {
+                        sound.pause();
+                    } else {
+                        sound.play();
+                    }
+                };
+
+
+                $scope.stop = () => {
+                    if (sound == null || !$scope.controlsEnabled) return;
+                    sound.stop();
+                };
+
 
                 $ctrl.$onChanges = function (changes) {
-                    if (changes.path) {
+                    if (changes.path || changes.outputDevice) {
+                        loadSound();
+                    }
+                    if (changes.volume) {
+                        if (sound != null) {
+                            let newVolume = changes.volume.currentValue;
+                            if (!isNaN(newVolume)) {
 
+                                if (newVolume > 10) {
+                                    newVolume = 1;
+                                } else if (newVolume > 0) {
+                                    newVolume = newVolume / 10;
+                                }
+                                sound.volume(newVolume);
+                            }
+                        }
+                    }
+                };
+
+                $ctrl.$onDestroy = function() {
+                    $interval.cancel(seekPositionTimer);
+                    if (sound != null) {
+                        sound.unload();
                     }
                 };
 
 
 
                 $ctrl.$onInit = function() {
-
+                    loadSound();
                 };
             }
         });
