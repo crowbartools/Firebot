33f8ba6167eba9b484a70abe71c7a29e5dcb24bd
Erik Bigler <erikbigler@gmail.com>
2018-10-30 13:50:44 -0700

    start work on updating v5 effects to use new v4 stylings

--- gui/app/directives/effect-option-settings/eosContainer.js
+++ gui/app/directives/effect-option-settings/eosContainer.js
@@ -8,12 +8,12 @@
         },
         transclude: true,
         template: `
-                <div class="effect-setting-container" ng-class="{ 'setting-padtop' : $ctrl.padTop }">
-                    <div class="effect-specific-title" ng-if="$ctrl.header">
-                        <h4>{{$ctrl.header}}</h4>
-                    </div>
-                    <div class="effect-setting-content" ng-transclude></div>
+            <div class="effect-setting-container" ng-class="{ 'setting-padtop' : $ctrl.padTop }">
+                <div class="effect-specific-title" ng-if="$ctrl.header">
+                    <h4>{{$ctrl.header}}</h4>
                 </div>
-            `
+                <div class="effect-setting-content" ng-transclude></div>
+            </div>
+        `
     });
 }());
