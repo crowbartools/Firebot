b988e79585a77f8f71607e352b70529c96c9bd54
Erik Bigler <erik.bigler@mindbodyonline.com>
2018-12-17 17:43:46 -0800

    create mixplayservice

---
+++ gui/app/services/mixplayService.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function() {
+
+    const profileManager = require("../../lib/common/profile-manager.js");
+
+    angular
+        .module("firebotApp")
+        .factory("mixplayService", function($rootScope, listenerService, logger) {
+            let service = {};
+
+
+            return service;
+        });
+}());
