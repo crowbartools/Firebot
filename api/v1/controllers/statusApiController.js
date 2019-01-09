ebb842a8293f50caeb8bb2e3f2848d3a77b28e32
Erik Bigler <erik.bigler@mindbodyonline.com>
2019-01-09 00:43:56 -0800

    add chat and constellation status to status endpoint

--- api/v1/controllers/statusApiController.js
+++ api/v1/controllers/statusApiController.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const mixerInteractive = require('../../../lib/common/mixer-interactive.js');
+const chat = require('../../../lib/common/mixer-chat.js');
+const constellation = require("../../../lib/live-events/mixer-constellation");
 
 exports.getStatus = function(req, res) {
     let status = {
         connections: {
-            interactive: mixerInteractive.getInteractiveStatus()
+            interactive: mixerInteractive.getInteractiveStatus(),
+            chat: chat.getChatStatus(),
+            constellation: constellation.getConstellationStatus()
         }
     };
     res.json(status);
