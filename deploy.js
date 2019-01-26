f1a75341941564103f96b467742eca7219903044
SReject <sreject@comcast.net>
2019-01-26 04:12:27 -0500

    Output deploy script state

--- deploy.js
+++ deploy.js
@@ -8,21 +8,27 @@ const data = JSON.stringify({
     }
 });
 
-const options = {
-    hostname: 'api.travis-ci.org',
-    port: 443,
-    path: '/repo/crowbartools%2Ffirebot-dev-builds/requests',
-    method: 'POST',
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Travis-API-Version": "3",
-        "Authorization": `token ${process.env.DEVBUILD_API_KEY}`,
-        "Content-length": data.length
+const req = https.request(
+    'https://api.travis-ci.org/repo/crowbartools%2Ffirebot-dev-builds/requests',
+    {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Travis-API-Version": "3",
+            "Authorization": `token ${process.env.DEVBUILD_API_KEY}`,
+            "Content-length": data.length
+        }
+    },
+    res => {
+        if (res.statusCode !== 200) {
+            throw new Error(`server responded with: ${res.statusCode}`);
+        }
+
+        req.on('data', data => console.log(data));
     }
-};
-
-
-const req = https.request(options);
+).on('error', err => {
+    console.error(err.message);
+});
 req.write(data);
 req.end();
