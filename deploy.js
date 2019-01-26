34a32029b5356119e66763988aadcd1a38fc6e66
SReject <sreject@comcast.net>
2019-01-26 00:59:00 -0500

    setuo automated builds

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+const data = JSON.stringify({
+    request: {
+        message: 'Build triggered from FireBot',
+        branch: 'source'
+    }
+});
+
+const options = {
+    hostname: 'api.travis-ci.org',
+    port: 443,
+    path: '/repo/crowbartools%2Ffirebot-dev-builds/requests',
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Travis-API-Version": "3",
+        "Authorization": `token ${process.env.DEVBUILD_API_KEY}`,
+        "Content-length": data.length
+    }
+};
+
+
+const req = https.request(options);
+req.write(data);
+req.end();
