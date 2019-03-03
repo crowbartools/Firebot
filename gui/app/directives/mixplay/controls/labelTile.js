559ece36f1643ce1ef53d57053d3e402b9b3cbbd
Erik Bigler <erikbigler@gmail.com>
2019-03-03 01:06:25 -0800

    fix label preview

--- gui/app/directives/mixplay/controls/labelTile.js
+++ gui/app/directives/mixplay/controls/labelTile.js
@@ -8,7 +8,7 @@
             },
             template: `
                 <div class="mixer-label-container">
-                    <div ng-style="$ctrl.getLabelStyle()">{{$ctrl.control.text}}</div>
+                    <div ng-style="$ctrl.getLabelStyle()">{{$ctrl.control.mixplay.text}}</div>
                 </div>                        
             `,
             controller: function() {
@@ -16,20 +16,22 @@
 
                 $ctrl.getLabelStyle = function() {
                     let style = {};
-                    if ($ctrl.control.textSize) {
-                        style['font-size'] = $ctrl.control.textSize;
-                    }
-                    if ($ctrl.control.textColor) {
-                        style['color'] = $ctrl.control.textColor;
-                    }
-                    if ($ctrl.control.bold) {
-                        style['font-weight'] = '700';
-                    }
-                    if ($ctrl.control.underline) {
-                        style['text-decoration'] = 'underline';
-                    }
-                    if ($ctrl.control.italic) {
-                        style['font-style'] = 'italic';
+                    if ($ctrl.control.mixplay) {
+                        if ($ctrl.control.mixplay.textSize) {
+                            style['font-size'] = $ctrl.control.mixplay.textSize;
+                        }
+                        if ($ctrl.control.mixplay.textColor) {
+                            style['color'] = $ctrl.control.mixplay.textColor;
+                        }
+                        if ($ctrl.control.mixplay.bold) {
+                            style['font-weight'] = '700';
+                        }
+                        if ($ctrl.control.mixplay.underline) {
+                            style['text-decoration'] = 'underline';
+                        }
+                        if ($ctrl.control.mixplay.italic) {
+                            style['font-style'] = 'italic';
+                        }
                     }
                     return style;
                 };
