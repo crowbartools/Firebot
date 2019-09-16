d0b85de35dfbcf6af01fd02370b095475e362e58
Erik Bigler <erik.bigler@mindbodyonline.com>
2019-09-16 16:32:31 -0700

    update customscript processor logic

--- backend/common/handlers/custom-scripts/customScriptProcessor.js
+++ backend/common/handlers/custom-scripts/customScriptProcessor.js
@@ -10,9 +10,9 @@ const {
     app
 } = require('electron');
 
-const commandRouter = require('../../../chat/command-router');
 const effectRunner = require('../../effect-runner.js');
 const accountAccess = require('../../account-access');
+const uuidv1 = require("uuid/v1");
 const {EffectType, TriggerType } = require('../../EffectType.js');
 
 
@@ -29,57 +29,6 @@ function getUserRoles(participant) {
             });
         } else {
             resolve([]);
-        }
-    });
-}
-
-//runs all commands in a list recursively. It waits for the effects of each command to finish before running the next.
-function runCommands(
-    commandsList,
-    index,
-    commandsCache,
-    username,
-    firebotJson
-) {
-    return new Promise(resolve => {
-        if (index >= commandsList.length) {
-            resolve();
-        }
-
-        let c = commandsList[index];
-
-        let savedCommand = commandsCache.Active[c.id];
-
-        if (savedCommand == null) {
-            savedCommand = commandsCache.Inactive[c.id];
-        }
-
-        if (savedCommand != null) {
-            let userCommand = {
-                cmd: { value: savedCommand.trigger },
-                args: c.args ? c.args : []
-            };
-
-            commandRouter
-                .processChatEffects(
-                    c.sender ? c.sender : username,
-                    false,
-                    savedCommand,
-                    firebotJson,
-                    null,
-                    userCommand,
-                    false
-                )
-                .then(() => {
-                    let newIndex = index + 1;
-                    runCommands(
-                        commandsList,
-                        newIndex,
-                        commandsCache,
-                        username,
-                        firebotJson
-                    ).then(resolve);
-                });
         }
     });
 }
@@ -164,78 +113,61 @@ function scriptProcessor(effect, trigger) {
                     moment: require('moment'),
                     logger: logger,
                     chat: chat,
-                    interactive: require("../../mixer-interactive"),
+                    mixplay: require("../../../interactive/mixplay"),
                     utils: require("../../../utility")
                 };
 
-                let response;
-
-                let isV2Script = customScript.run.length === 1;
-                if (isV2Script) {
-                    //simpify parameters
-                    let simpleParams = {};
-                    if (parameters != null) {
-                        Object.keys(parameters).forEach(k => {
-                            let param = parameters[k];
-                            if (param != null) {
-                                simpleParams[k] =
-                  param.value == null && param.value !== ""
-                      ? param.default
-                      : param.value;
-                            }
-                        });
-                    }
-
-                    let accounts = {};
-                    try {
-                        accounts = profileManager.getJsonDbInProfile("/auth").getData("/");
-                    } catch (err) {} // eslint-disable-line no-empty
-
-                    let runRequest = {
-                        modules: modules,
-                        buttonId: button != null ? button.controlId : null, // only here for backwards compat
-                        button:
-              button != null
-                  ? {
-                      name: button.controlId,
-                      meta: button.meta || {}
-                  }
-                  : null,
-                        command:
-              userCommand != null
-                  ? {
-                      trigger: userCommand.cmd.value,
-                      args: userCommand.args.filter(a => a != null && a !== "")
-                  }
-                  : null,
-                        username: username, // only here for backwards compat
-                        user: {
-                            name: username
-                        },
-                        firebot: {
-                            accounts: accountAccess.getAccounts(),
-                            settings: {
-                                webSocketPort: settings.getWebSocketPort(),
-                                webServerPort: settings.getWebServerPort()
-                            },
-                            currentInteractiveBoardId: settings.getLastBoardName(),
-                            version: app.getVersion()
-                        },
-                        parameters: simpleParams,
-                        trigger: trigger,
-                        triggerType: triggerType // only here for backwards compat
-                    };
-
-                    response = getUserRoles(participant)
-                        .then(roles => {
-                            runRequest.user.roles = roles;
-                        })
-                        .then(() => {
-                            return customScript.run(runRequest);
-                        });
-                } else {
-                    response = customScript.run(button.controlId, username, modules);
+                //simpify parameters
+                let simpleParams = {};
+                if (parameters != null) {
+                    Object.keys(parameters).forEach(k => {
+                        let param = parameters[k];
+                        if (param != null) {
+                            simpleParams[k] = param.value == null && param.value !== ""
+                                ? param.default
+                                : param.value;
+                        }
+                    });
                 }
+
+                let runRequest = {
+                    modules: modules,
+                    button: button != null
+                        ? {
+                            name: button.controlId,
+                            meta: button.meta || {}
+                        }
+                        : null,
+                    command: userCommand != null
+                        ? {
+                            trigger: userCommand.cmd.value,
+                            args: userCommand.args.filter(a => a != null && a !== "")
+                        }
+                        : null,
+                    user: {
+                        name: username
+                    },
+                    firebot: {
+                        accounts: accountAccess.getAccounts(),
+                        settings: {
+                            webSocketPort: settings.getWebSocketPort(),
+                            webServerPort: settings.getWebServerPort()
+                        },
+                        currentInteractiveBoardId: settings.getLastBoardName(),
+                        version: app.getVersion()
+                    },
+                    parameters: simpleParams,
+                    trigger: trigger,
+                    triggerType: triggerType // only here for backwards compat
+                };
+
+                let response = getUserRoles(participant)
+                    .then(roles => {
+                        runRequest.user.roles = roles;
+                    })
+                    .then(() => {
+                        return customScript.run(runRequest);
+                    });
 
                 if (response) {
                     // Add a check to verify the response is a Promise
@@ -243,135 +175,58 @@ function scriptProcessor(effect, trigger) {
                     // Otherwise, just do nothing.
                     if (response instanceof Promise) {
                         response.then(data => {
-                            if (isV2Script) {
-                                /* In a V2 script, we are expecting a response like this:
-                {
-                   success: boolean,
-                   errorMessage: string (optional),
-                   effects: [
-                    {
-                      type: EffectType.CHAT,
-                      message: "This is a chat message, yay.",
-                      whisper: string (user name to whisper to),
-                      chatter: string ("bot" or "streamer")
-                    },
-                    {
-                      type: EffectType.PLAY_SOUND,
-                      filepath: "c:\asdasd\asdasd\sound.mp3",
-                      volume: 5
-                    }
-                  ],
-                  callback: Function
-                }
-                */
 
-                                if (data) {
-                                    let responseObject = data;
+                            if (data) {
+                                let responseObject = data;
 
-                                    if (responseObject.success === true) {
-                                        if (typeof responseObject.callback !== "function") {
-                                            responseObject.callback = () => {};
-                                        }
+                                if (responseObject.success === true) {
+                                    if (typeof responseObject.callback !== "function") {
+                                        responseObject.callback = () => {};
+                                    }
 
-                                        let effects = responseObject.effects;
+                                    let effects = responseObject.effects;
 
-                                        let builtEffects = {};
-                                        let effectCount = 1;
-                                        effects.forEach(e => {
-                                            let type = e.type;
-                                            if (
-                                                type !== null &&
-                                                type !== undefined &&
-                                                type !== "" &&
-                                                Object.values(EffectType).includes(type)
-                                            ) {
-                                                builtEffects[effectCount.toString()] = e;
-                                            } else {
-                                                renderWindow.webContents.send(
-                                                    "error",
-                                                    "Custom script tried to execute an unknown or unsupported effect type: " +
-                            type
-                                                );
+
+                                    if (effects) {
+
+                                        //filter out effects that do not have v5 types assigned
+                                        effects = effects.filter(e => e.type != null);
+
+                                        //generate id's for effects that dont have them
+                                        effects = effects.map(e => {
+                                            if (e.id == null) {
+                                                e.id = uuidv1();
                                             }
-
-                                            effectCount++;
+                                            return e;
                                         });
-
-                                        if (Object.keys(builtEffects).length > 0) {
-                                            //Run effects if there are any
-
-                                            let newTrigger = Object.assign({}, trigger);
-                                            newTrigger.type = TriggerType.CUSTOM_SCRIPT;
-                                            // Create request wrapper
-
-                                            let processEffectsRequest = {
-                                                trigger: newTrigger,
-                                                effects: builtEffects
-                                            };
-
-                                            effectRunner
-                                                .processEffects(processEffectsRequest)
-                                                .then(() => {
-                                                    responseObject.callback("effects");
-                                                });
-                                        }
-
-                                        if (responseObject.commands != null && responseObject.commands.length > 0) {
-                                            let commandsCache = chat.getCommandCache();
-
-                                            runCommands(
-                                                responseObject.commands,
-                                                0,
-                                                commandsCache,
-                                                username,
-                                                null
-                                            ).then(() => {
-                                                responseObject.callback("commands");
-                                            });
-                                        }
-                                    } else {
-                                        logger.error("Script failed. Sending error.");
-                                        logger.error(responseObject.errorMessage);
-                                        renderWindow.webContents.send(
-                                            "error",
-                                            "Custom script failed with the message: " +
-                        responseObject.errorMessage
-                                        );
                                     }
-                                }
-                            } else {
-                                /*
-                  Script V1 response data we are expecting is either nothing, or:
-                  {
-                   success: boolean,
-                      - Whether or not the script was successful
-                   message: string,
-                      - Message either shown as an error or potentially sent in chat
-                   chatAs: string or null
-                      - If this is "bot" or "streamer", we chat the message as the given chatter
-                   whisper: true or false/null
-                      - If this is true and the above is set to chat as someone, this will whisper
-                        to the participant instead
-                  }
-                */
-                                if (data) {
-                                    if (data.success === true && data.message) {
-                                        let chatAs = data.chatAs;
-                                        // We just need to check if chatAs is empty, everything else
-                                        // is handled by te .whipser and .broadcast calls.
-                                        if (chatAs) {
-                                            if (data.whisper === true) {
-                                                chat.whisper(chatAs, username, data.message);
-                                            } else {
-                                                chat.broadcast(chatAs, data.message);
+
+                                    //Run effects if there are any
+                                    if (effects && effects.length > 0) {
+
+                                        let newTrigger = Object.assign({}, trigger);
+                                        newTrigger.type = TriggerType.CUSTOM_SCRIPT;
+
+                                        // Create request wrapper
+                                        let processEffectsRequest = {
+                                            trigger: newTrigger,
+                                            effects: {
+                                                list: effects,
+                                                id: uuidv1()
                                             }
-                                        }
-                                    } else {
-                                        renderWindow.webContents.send(
-                                            "error",
-                                            "Custom script failed with the message: " + data.message
-                                        );
+                                        };
+
+                                        effectRunner
+                                            .processEffects(processEffectsRequest)
+                                            .then(() => {
+                                                responseObject.callback("effects");
+                                            });
                                     }
+
+                                } else {
+                                    logger.error("Script failed. Sending error.");
+                                    logger.error(responseObject.errorMessage);
+                                    renderWindow.webContents.send("error", "Custom script failed with the message: " + responseObject.errorMessage);
                                 }
                             }
                         });
@@ -379,18 +234,10 @@ function scriptProcessor(effect, trigger) {
                 }
             }, 1);
         } else {
-            renderWindow.webContents.send(
-                "error",
-                "Error running '" +
-          scriptName +
-          "', script does not contain a visible run fuction."
-            );
+            renderWindow.webContents.send("error", `Error running '${scriptName}', script does not contain a visible run fuction.`);
         }
     } catch (err) {
-        renderWindow.webContents.send(
-            "error",
-            "Error loading the script '" + scriptName + "'\n\n" + err
-        );
+        renderWindow.webContents.send("error", `Error loading the script '${scriptName}' \n\n ${err}`);
         logger.error(err);
     }
 }
