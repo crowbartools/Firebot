17908b7b42f9bf37b858d56ab48418eb45b5ab33
Firebottle <mage@gamingobscura.com>
2020-01-05 14:31:34 -0600

    Implement currency restriction.

--- backend/restrictions/builtin-restrictions-loader.js
+++ backend/restrictions/builtin-restrictions-loader.js
@@ -5,7 +5,9 @@ const restrictionsManager = require("./restriction-manager");
 exports.loadRestrictions = function() {
     const permissions = require("./builtin/permissions");
     const channelProgression = require("./builtin/channelProgression");
+    const channelCurrency = require("./builtin/channelCurrency");
 
     restrictionsManager.registerRestriction(permissions);
     restrictionsManager.registerRestriction(channelProgression);
+    restrictionsManager.registerRestriction(channelCurrency);
 };
---
+++ backend/restrictions/builtin/channelCurrency.js
@@ -0,0 +1,120 @@
+"use strict";
+
+const profileManager = require("../../common/profile-manager");
+
+const model = {
+    definition: {
+        id: "firebot:channelcurrency",
+        name: "Channel Currency",
+        description: "Restrict based on channel currency amounts.",
+        triggers: []
+    },
+    optionsTemplate: `
+        <div>
+            <div ng-show="hasCurrencies">
+                <div id="channelCurrency" class="mixplay-header" style="padding: 0 0 4px 0">
+                    Channel Currency
+                </div>
+                <div class="">
+                    <select class="fb-select" ng-model="restriction.selectedCurrency" ng-options="currency as currency.name for currency in currencies track by currency.id"></select>
+                </div>
+
+                <div id="channelCurrencyOption" class="mixplay-header" style="padding: 0 0 4px 0">
+                    Comparison
+                </div>
+                <div class="">
+                    <select class="fb-select" ng-model="restriction.comparison">
+                        <option label="Less than (or equal to)" value="less" selected="selected">Less than (or equal to)</option>
+                        <option label="Greater than (or equal to)" value="greater">Greater than (or equal to)</option>
+                        <option label="Equal to" value="equal">Equal to</option>
+                    </select>
+                </div>
+
+                <div id="channelCurrencyAmount" class="mixplay-header" style="padding: 0 0 4px 0">
+                    Amount
+                </div>
+                <div class="form-group">
+                    <input class="fb-control fb-select" style="color:black; padding-left: .3em" type="number" ng-model="restriction.amount">
+                </div>
+            </div>
+            <div ng-show="!hasCurrencies">
+                <p>You have not created any currencies to use with this restriction!</p>
+            </div>
+        </div>
+    `,
+    optionsController: ($scope) => {
+        // The currency settings.
+        let currencyDb = profileManager.getJsonDbInProfile("/currency/currency");
+
+        // Get list of currencies
+        $scope.currencies = [];
+        let currencyData = currencyDb.getData("/");
+        Object.keys(currencyData).forEach(function(k) {
+            $scope.currencies.push(currencyData[k]);
+        });
+
+        console.log($scope.currencies);
+
+        // Do we have any currencies?
+        $scope.hasCurrencies = $scope.currencies.length > 0;
+    },
+    optionsValueDisplay: (restriction) => {
+        let comparison = restriction.comparison.toLowerCase();
+        let currency = restriction.selectedCurrency;
+        let amount = restriction.amount;
+
+        if (comparison === "less") {
+            comparison = "less than";
+        } else if (comparison === "greater") {
+            comparison = "greater than";
+        } else if (comparison === "equal") {
+            comparison = "equal to";
+        }
+
+        return currency.name + " is " + comparison + " " + amount;
+    },
+    /*
+      function that resolves/rejects a promise based on if the restriction critera is met
+    */
+    predicate: (triggerData, restrictionData) => {
+        return new Promise(async (resolve, reject) => {
+            let passed = false;
+            let currencyDatabase = require("../../database/currencyDatabase");
+            let username = triggerData.metadata.username;
+            let userCurrency = await currencyDatabase.getUserCurrencyAmount(username, restrictionData.selectedCurrency.id);
+
+            if (userCurrency !== false) {
+                let comparison = restrictionData.comparison;
+                let currencyAmount = restrictionData.amount;
+
+                if (comparison === "less" && userCurrency <= currencyAmount) {
+                    passed = true;
+                }
+
+                if (comparison === "greater" && userCurrency >= currencyAmount) {
+                    passed = true;
+                }
+
+                if (comparison === "equal" && userCurrency === currencyAmount) {
+                    passed = true;
+                }
+            }
+
+
+            if (passed) {
+                resolve();
+            } else {
+                reject("You don't meet the currency requirements for this action.");
+            }
+        });
+    },
+    /*
+        called after all restrictions in a list are met. Do logic such as deducting currency here.
+    */
+    onSuccessful: (triggerData, restrictionData) => {
+
+    }
+
+};
+
+module.exports = model;
