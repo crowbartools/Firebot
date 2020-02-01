4d5359ec1aa760dd29a1a4f7d3904f6aba5abb15
Firebottle <mage@gamingobscura.com>
2020-02-01 10:47:44 -0600

    Read file updates

--- backend/variables/builtin/readFile.js
+++ backend/variables/builtin/readFile.js
@@ -12,7 +12,7 @@ const model = {
     definition: {
         handle: "readFile",
         usage: "readFile[\"path/to/file.txt\"]",
-        description: "Read contents of a text file. Optionally include 'true' as a second argument to select a random line.",
+        description: "Read contents of a text file. Optionally include 'true' as a second argument to select a random line, or a number to read a specific line.",
         possibleDataOutput: [OutputDataType.TEXT]
     },
     evaluator: (_, filePath, randomLine) => {
@@ -20,11 +20,28 @@ const model = {
         if (filePath === null || !filePath.endsWith(".txt")) return "[File Path Error]";
         try {
             let contents = fs.readFileSync(filePath, "utf8");
-            if (randomLine) {
+            let filteredLines = [];
+
+            if (randomLine === "true" || !isNaN(randomLine)) {
                 let lines = contents.replace(/\r\n/g, "\n").split("\n");
-                let randIndex = util.getRandomInt(0, lines.length - 1);
-                return lines[randIndex];
+                filteredLines = lines.filter(function (line) {
+                    if (line != null && line.trim() !== "") {
+                        return line;
+                    }
+                });
             }
+
+            // Get line at random.
+            if (randomLine === "true") {
+                let randIndex = util.getRandomInt(0, filteredLines.length - 1);
+                return filteredLines[randIndex];
+            }
+
+            if (!isNaN(randomLine)) {
+                randomLine = randomLine - 1;
+                return filteredLines[randomLine];
+            }
+
             return contents;
         } catch (err) {
             logger.error("error reading file", err);
