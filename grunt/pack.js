2943643c815110943b154a7d904e263de2b40627
SReject <sreject@comcast.net>
2020-02-19 17:43:48 -0500

    Fixes packaging

--- grunt/pack.js
+++ grunt/pack.js
@@ -1,39 +1,37 @@
 'use strict';
 module.exports = function (grunt) {
-    let base = [
-        'electron-packager . Firebot',
-        '--out=./dist/pack/',
+    let flags = [
+        '--out="./dist/pack"',
         '--arch=x64',
         '--electronVersion=7.1.9',
         '--js-flags="--harmony"',
-        '--asar.unpack="moderation-ervice.js"',
+        '--asar.unpack="moderation-service.js"',
         '--prune',
         '--overwrite',
-        '--version-string.ProductName="Firebot v5"',
-        '--executable-name="Firebot v5"',
+        '--version-string.ProductName="Firebot V5"',
+        '--executable-name="Firebot V5"',
         '--icon="./gui/images/icon_transparent.ico"',
         '--ignore=/.github',
         '--ignore=/.vscode',
-        '--ignore=/dist',
-        '--ignore=/doc',
         '--ignore=/grunt',
-        '--ignore=/profiles',
-        '--ignore=/resources'
+        '--ignore=/resources',
+        '--ignore=/doc',
+        '--ignore=/profiles'
     ].join(' ');
 
     grunt.config.merge({
         shell: {
             packwin64: {
-                command: `${base} --platform=win32`
+                command: `npx electron-packager . Firebot --platform=win32 ${flags}`
             },
             packlinux64: {
-                command: `${base} --platform=linux`
+                command: `npx electron-packager . Firebot --platform=linux ${flags}`
             }
         }
     });
 
     grunt.registerTask('pack', function (scope) {
-        scope = scope || grunt.config.get('platform') || 'win32';
+        scope = scope || grunt.config.get('platform') || 'win64';
 
         if (scope === 'win64' || scope === 'linux64') {
             grunt.task.run([
