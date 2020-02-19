5f43f1c541ab9f32cab7d3d70b0cc077d597fc8a
SReject <sreject@comcast.net>
2020-02-19 05:11:20 -0500

    Add grunt installer creation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,36 +5,19 @@ module.exports = function(grunt) {
         grunt.fail.fatal(new Error('Unknown Platform'), 1);
     }
 
-    const WORKING_DIR = process.cwd();
-
     // Project configuration.
     grunt.initConfig({
         platform: PLATFORM,
         pkg: grunt.file.readJSON('package.json'),
-        'create-windows-installer': {
-            x64: {
-                appDirectory: WORKING_DIR + '/dist/Firebot-win32-x64',
-                outputDirectory: WORKING_DIR + '/dist/installer64',
-                loadingGif: WORKING_DIR + '/gui/images/animated.gif',
-                iconUrl: WORKING_DIR + '/gui/images/icon_transparent.ico',
-                setupIcon: WORKING_DIR + '/gui/images/icon_transparent.ico',
-                exe: "Firebot V5.exe",
-                title:" Firebot V5",
-                setupExe: "FirebotV5Setup.exe",
-                noMsi: true
-            }
-        }
     });
 
     // Register shell executor
     grunt.loadNpmTasks('grunt-shell');
-
-    // Load installer builder.
-    grunt.loadNpmTasks('grunt-electron-installer');
 
     require('./grunt/cleanup.js')(grunt);
     require('./grunt/sass.js')(grunt);
     require('./grunt/lint.js')(grunt);
     require('./grunt/copy.js')(grunt);
     require('./grunt/pack.js')(grunt);
+    require('./grunt/compile.js')(grunt);
 };
---
+++ grunt/compile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const path = require('path');
+module.exports = function (grunt) {
+    grunt.config.merge({
+        'create-windows-installer': {
+            win64: {
+                appDirectory: path.join(__dirname, '../dist/pack/Firebot-win32-x64/'),
+                outputDirectory: path.join(__dirname, '../dist/install/win64/'),
+                loadingGif: path.join(__dirname, '../gui/images/animated.gif'),
+                iconUrl: path.join(__dirname, '../gui/images/icon_transparent.ico'),
+                setupIcon: path.join(__dirname, '../gui/images/icon_transparent.ico'),
+                exe: "Firebot v5.exe",
+                title: " Firebot v5",
+                setupExe: "FirebotV5Setup.exe",
+                noMsi: true
+            }
+        },
+        compress: {
+            linux64: {
+                options: {
+                    archieve: path.join(__dirname, '../dist/install/linux64/Firebot-linux-x64.tar.gz'),
+                    mode: 'tgz'
+                },
+                files: [{expand: true, dest: '/', src: ['**'], cmd: path.join(__dirname, '../dist/pack/Firebot-linux-x64')}]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-electron-installer');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    grunt.registerTask('compile', function (scope) {
+        scope = scope || grunt.option('platform') || 'win64';
+
+        if (scope === 'win64') {
+            grunt.task.run('pack', 'create-windows-installer:win64');
+
+        } else if (scope === 'linux64') {
+            grunt.task.run('pack:linux64', 'compress:linux');
+
+        } else {
+            grunt.task.fatal(new Error('invalid platform'), 1);
+        }
+    });
+};
