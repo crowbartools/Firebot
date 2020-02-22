b4a0af0e29eb62b71aec76c291000886529c6c49
ebiggz <erikbigler@gmail.com>
2020-02-22 02:16:17 -0600

    remove redditor package and cleanup random reddit image effect UI

--- backend/effects/builtin/randomRedditImage.js
+++ backend/effects/builtin/randomRedditImage.js
@@ -1,20 +1,12 @@
 "use strict";
 
-const { settings } = require("../../common/settings-access");
-const resourceTokenManager = require("../../resourceTokenManager");
 const redditProcessor = require("../../common/handlers/redditProcessor");
 
 const { ControlKind, InputEvent } = require('../../interactive/constants/MixplayConstants');
 const effectModels = require("../models/effectModels");
 const { EffectDependency, EffectTrigger } = effectModels;
 
-/**
- * The Reddit effect
- */
-const reddit = {
-    /**
-   * The definition of the Effect
-   */
+const model = {
     definition: {
         id: "firebot:randomReddit",
         name: "Random Reddit Image",
@@ -27,24 +19,16 @@ const reddit = {
             EffectTrigger.ALL
         )
     },
-    /**
-   * Global settings that will be available in the Settings tab
-   */
     globalSettings: {},
-    /**
-   * The HTML template for the Options view (ie options when effect is added to something such as a button.
-   * You can alternatively supply a url to a html file via optionTemplateUrl
-   */
     optionsTemplate: `
-    <div class="effect-setting-container">
-        <div class="effect-specific-title"><h4>Which subreddit should we pull images from?</h4></div>
+    <eos-container header="Subreddit Name">
         <div class="input-group">
             <span class="input-group-addon" id="reddit-effect-type">To</span>
             <input ng-model="effect.reddit" type="text" class="form-control" id="reddit-setting" aria-describedby="chat-text-effect-type" placeholder="puppies">
         </div>
-    </div>
-    <div class="effect-setting-container" ng-if="effect.reddit !== null && effect.reddit !== 'Pick one'">
-        <div class="effect-specific-title"><h4>Where should we send this?</h4></div>
+    </eos-container>
+
+    <eos-container header="Output Location" pad-top="true" ng-if="effect.reddit !== null && effect.reddit !== 'Pick one'">
         <div class="controls-fb-inline" style="padding-bottom: 5px;">
             <label class="control-fb control--radio">Chat
                 <input type="radio" ng-model="effect.show" value="chat"/> 
@@ -59,69 +43,69 @@ const reddit = {
                 <div class="control__indicator"></div>
             </label>
         </div>
-    </div>
+    </eos-container>
 
     <div class="effect-reddit-settings" ng-if="effect.show === 'chat' || effect.show ==='both'">
-        <eos-chatter-select effect="effect" title="Who should I send this to chat as?"></eos-chatter-select>
+        <eos-chatter-select effect="effect" title="Chatter" class="setting-padtop"></eos-chatter-select>
     </div>
 
     <div class="effect-reddit-settings" ng-if="effect.show === 'overlay' || effect.show ==='both'">
-    <eos-overlay-position effect="effect" class="setting-padtop"></eos-overlay-position>
-    <eos-enter-exit-animations effect="effect" class="setting-padtop"></eos-enter-exit-animations>
-    <div class="effect-setting-container setting-padtop">
-        <div class="effect-specific-title"><h4>Dimensions</h4></div>
-        <div class="effect-setting-content">
-            <div class="input-group">
-                <span class="input-group-addon">Width</span>
-                <input 
-                type="number" 
-                class="form-control" 
-                aria-describeby="image-width-setting-type" 
-                type="number"
-                ng-model="effect.width"
-                placeholder="px">
-                <span class="input-group-addon">Height</span>
-                <input 
-                type="number" 
-                class="form-control" 
-                aria-describeby="image-height-setting-type" 
-                type="number"
-                ng-model="effect.height"
-                placeholder="px">
+        <eos-overlay-position effect="effect" class="setting-padtop"></eos-overlay-position>
+        <eos-enter-exit-animations effect="effect" class="setting-padtop"></eos-enter-exit-animations>
+        <div class="effect-setting-container setting-padtop">
+            <div class="effect-specific-title"><h4>Dimensions</h4></div>
+            <div class="effect-setting-content">
+                <div class="input-group">
+                    <span class="input-group-addon">Width</span>
+                    <input 
+                    type="number" 
+                    class="form-control" 
+                    aria-describeby="image-width-setting-type" 
+                    type="number"
+                    ng-model="effect.width"
+                    placeholder="px">
+                    <span class="input-group-addon">Height</span>
+                    <input 
+                    type="number" 
+                    class="form-control" 
+                    aria-describeby="image-height-setting-type" 
+                    type="number"
+                    ng-model="effect.height"
+                    placeholder="px">
+                </div>
             </div>
         </div>
-    </div>
-    <div class="effect-setting-container setting-padtop">
-        <div class="effect-specific-title"><h4>Duration</h4></div>
-        <div class="effect-setting-content">
-            <div class="input-group">
-                <span class="input-group-addon">Seconds</span>
-                <input 
-                type="text" 
-                class="form-control" 
-                aria-describedby="image-length-effect-type" 
-                type="number"
-                ng-model="effect.length">
+        <div class="effect-setting-container setting-padtop">
+            <div class="effect-specific-title"><h4>Duration</h4></div>
+            <div class="effect-setting-content">
+                <div class="input-group">
+                    <span class="input-group-addon">Seconds</span>
+                    <input 
+                    type="text" 
+                    class="form-control" 
+                    aria-describedby="image-length-effect-type" 
+                    type="number"
+                    ng-model="effect.length">
+                </div>
             </div>
         </div>
-    </div>
-    <eos-overlay-instance effect="effect" class="setting-padtop"></eos-overlay-instance>   
+        <eos-overlay-instance effect="effect" class="setting-padtop"></eos-overlay-instance>   
     </div>
 
-    <div class="effect-info alert alert-danger">
-    Warning: This effect pulls random images from subreddits. Highly moderated subreddits are fairly safe, but there is always the chance of naughty pictures. Just a warning!
-    </div>
+    <eos-container pad-top="true">
+        <div class="effect-info alert alert-danger">
+        Warning: This effect pulls random images from subreddits. Highly moderated subreddits are fairly safe, but there is always the chance of naughty pictures. Just a warning!
+        </div>
+    </eos-container>
+    
     `,
-    /**
-   * The controller for the front end Options
-   */
-    optionsController: ($scope, listenerService) => {
+    optionsController: ($scope) => {
 
+        if ($scope.effect.show == null) {
+            $scope.effect.show = "chat";
+        }
 
     },
-    /**
-   * When the effect is triggered by something
-   */
     optionsValidator: effect => {
         let errors = [];
         if (effect.reddit == null) {
@@ -129,35 +113,16 @@ const reddit = {
         }
 
         if (effect.show == null) {
-            errors.push("Please select a places to show the API results.");
+            errors.push("Please select a place to show the API results.");
         }
         return errors;
     },
-    /**
-   * When the effect is triggered by something
-   */
     onTriggerEvent: event => {
-        return new Promise((resolve, reject) => {
+        return new Promise(resolve => {
             redditProcessor.go(event.effect);
             resolve(true);
         });
-    },
-    /**
-   * Code to run in the overlay
-   */
-    overlayExtension: {
-        dependencies: {
-            css: [],
-            js: []
-        },
-        event: {
-            name: "redditEffect",
-            onOverlayEvent: event => {
-                // The API Effect can sometimes show images in the overlay.
-                // As part of this we use the showImage event.
-            }
-        }
     }
 };
 
-module.exports = reddit;
+module.exports = model;
