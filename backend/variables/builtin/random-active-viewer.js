487216d5facc58003f7ea3edd1b51b2e81f96396
TheLastMage <17416635+LastMageTV@users.noreply.github.com>
2020-02-29 17:58:31 -0600

    Optional argument to get an active user in a custom role. (#718)
    * Optional argument to get an active user in a custom role.
    
    * Remove example of optional parameter.

--- backend/variables/builtin/random-active-viewer.js
+++ backend/variables/builtin/random-active-viewer.js
@@ -2,23 +2,45 @@
 const util = require("../../utility");
 const logger = require("../../logwrapper");
 const activeViewerHandler = require('../../roles/role-managers/active-chatters');
+const customRoleManager = require('../../roles/custom-roles-manager');
 
 const { OutputDataType } = require("../../../shared/variable-contants");
 
 const model = {
     definition: {
         handle: "randomActiveViewer",
-        description: "Get a random active viewer in chat.",
+        usage: "randomActiveViewer",
+        description: "Get a random active chatter. Optionally include a custom role name to get an active user in that role.",
         possibleDataOutput: [OutputDataType.TEXT]
     },
-    evaluator: async () => {
+    evaluator: async (_, roleName) => {
         logger.debug("Getting random active viewer...");
 
         let activeViewers = activeViewerHandler.getActiveChatters();
 
-        if (activeViewers && activeViewers.length > 0) {
+        if (activeViewers == null || activeViewers.length === 0) {
+            return "[Unable to get random active user]";
+        }
+
+        if (roleName == null) {
             let randIndex = util.getRandomInt(0, activeViewers.length - 1);
             return activeViewers[randIndex].username;
+        }
+
+        if (roleName != null) {
+            let customRole = customRoleManager.getRoleByName(roleName);
+            if (customRole == null) {
+                return "[Unable to get random active user]";
+            }
+
+            let customRoleUsers = customRole.viewers;
+            if (customRoleUsers.length === 0) {
+                return "[Unable to get random active user]";
+            }
+
+            let roleIntersection = activeViewers.filter(user => customRoleUsers.includes(user.username));
+            let randIndex = util.getRandomInt(0, roleIntersection.length - 1);
+            return roleIntersection[randIndex].username;
         }
 
         return "[Unable to get random active user]";
