476c403f1ff29d7342ab03c78bc9fe4842c77dcd
Firebottle <mage@gamingobscura.com>
2020-03-08 20:46:21 -0500

    Refactor prebuilt api processors.

--- backend/common/handlers/redditProcessor.js
+++ backend/common/handlers/redditProcessor.js
@@ -1,11 +1,7 @@
 "use strict";
 
-const chat = require("../mixer-chat.js");
-const mediaProcessor = require("./mediaProcessor");
-const settings = require("../settings-access").settings;
 const logger = require("../../logwrapper");
-const webServer = require("../../../server/httpServer");
-const request = require("request");
+const axios = require("axios");
 
 function postPicker(posts) {
     while (posts.length) {
@@ -28,29 +24,22 @@ function postPicker(posts) {
     return false;
 }
 
-function getSubredditData(subName) {
-    return new Promise(resolve => {
-        let normalizedSubName = subName.replace("/r/", '').replace("r/", '');
-        request.get(`https://www.reddit.com/r/${normalizedSubName}/hot.json?count=15&raw_json=1`, { json: true }, (error, response, body) => {
-            if (response.statusCode === 200) {
-                resolve(body && body.data && body.data.children);
-            } else {
-                logger.warning(`Error getting subreddit ${subName}`, error);
-                resolve(null);
-            }
+async function getSubredditData(subName) {
+    let normalizedSubName = subName.replace("/r/", '').replace("r/", '');
+    let url = "https://www.reddit.com/r/" + normalizedSubName + "/hot.json?count=15&raw_json=1";
+
+    return await axios.get(url)
+        .then(function(response) {
+            return response.data.data.children;
+        })
+        .catch(function(err) {
+            logger.warning(`Error getting subreddit ${subName}`, err);
         });
-    });
 }
 
 // Pulls a random image from a subreddit.
-async function randomReddit(effect) {
-    let chatter = effect.chatter;
-
-    // Take the user input subreddit and clean it up incase they included /r/.
-    let subName = effect.reddit;
-
-    let subData = await getSubredditData(subName);
-
+async function randomImageFromSubReddit(subreddit) {
+    let subData = await getSubredditData(subreddit);
 
     if (subData == null) {
         logger.error("Couldn't find any valid posts in the subreddit.");
@@ -58,70 +47,22 @@ async function randomReddit(effect) {
             "error",
             "Couldn't find any valid posts in the subreddit."
         );
-        return false;
+        return "";
     }
 
     // Get our random post image url.
-    let url = postPicker(subData);
-    if (url === false) {
+    let imageUrl = postPicker(subData);
+    if (imageUrl === false) {
         logger.error("Couldn't find any valid posts in the subreddit.");
         renderWindow.webContents.send(
             "error",
             "Couldn't find any valid posts in the subreddit."
         );
-        return false;
+        return "";
     }
 
-    // Okay, now let's send it off.
-    try {
-        if (effect.show === "chat" || effect.show === "both") {
-            // Send Chat
-            logger.info("Random Reddit: " + url);
-            chat.broadcast(chatter, "Random Reddit: " + url);
-        }
-
-        if (effect.show === "overlay" || effect.show === "both") {
-            // Send image to overlay.
-            let position = effect.position,
-                data = {
-                    url: url,
-                    imageType: "url",
-                    imagePosition: position,
-                    imageHeight: effect.height,
-                    imageWidth: effect.width,
-                    imageDuration: effect.length,
-                    enterAnimation: effect.enterAnimation,
-                    exitAnimation: effect.exitAnimation,
-                    customCoords: effect.customCoords
-                };
-
-            // Get random location.
-            if (position === "Random") {
-                position = mediaProcessor.getRandomPresetLocation();
-            }
-
-            if (settings.useOverlayInstances()) {
-                if (effect.overlayInstance != null) {
-                    if (
-                        settings
-                            .getOverlayInstances()
-                            .includes(effect.overlayInstance)
-                    ) {
-                        data.overlayInstance = effect.overlayInstance;
-                    }
-                }
-            }
-
-            // Send to overlay.
-            webServer.sendToOverlay("image", data);
-        }
-    } catch (err) {
-        renderWindow.webContents.send(
-            "error",
-            "There was an error sending a reddit picture."
-        );
-    }
+    return imageUrl;
 }
 
 // Export Functions
-exports.go = randomReddit;
+exports.getRandomImage = randomImageFromSubReddit;
