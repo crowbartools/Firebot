582d5510500d4c14c80ab9d8e12699f33b7ce286
ebiggz <erikbigler@gmail.com>
2020-03-19 02:40:36 -0500

    allow custom variables to parse json obj/arrays. Implements #740

--- backend/common/custom-variable-manager.js
+++ backend/common/custom-variable-manager.js
@@ -1,16 +1,96 @@
 
 "use strict";
+const logger = require('../logwrapper');
 const NodeCache = require("node-cache");
 
 const cache = new NodeCache({ stdTTL: 0, checkperiod: 5 });
 
+exports.addCustomVariable = (name, data, ttl = 0, propertyPath = null) => {
 
-exports.addCustomVariable = (name, data, ttl = 0) => {
-    cache.set(name, data, ttl === "" ? 0 : ttl);
+    //attempt to parse data as json
+    try {
+        data = JSON.parse(data);
+    } catch (error) {
+        //silently fail
+    }
+
+    let dataRaw = data ? data.toString().toLowerCase() : "null";
+    let dataIsNull = dataRaw === "null" || dataRaw === "undefined";
+
+    let currentData = cache.get(name);
+
+    if (propertyPath == null) {
+        let dataToSet = dataIsNull ? undefined : data;
+        if (currentData && Array.isArray(currentData) && !Array.isArray(data) && !dataIsNull) {
+            currentData.push(data);
+            dataToSet = currentData;
+        }
+        cache.set(name, dataToSet, ttl === "" ? 0 : ttl);
+    } else {
+        let currentData = cache.get(name);
+        if (!currentData) return;
+        try {
+            let cursor = currentData;
+            let pathNodes = propertyPath.split(".");
+            for (let i = 0; i < pathNodes.length; i++) {
+                let node = pathNodes[i];
+
+                // parse to int for array access
+                if (!isNaN(node)) {
+                    node = parseInt(node);
+                }
+
+                let isLastItem = i === pathNodes.length - 1;
+                if (isLastItem) {
+
+                    // if data recognized as null and cursor is an array, remove index instead of setting value
+                    if (dataIsNull && Array.isArray(cursor) && !isNaN(node)) {
+                        cursor.splice(node, 1);
+                    } else {
+                        //if next node is an array and we detect we are not setting a new array or removing array, then push data to array
+                        if (Array.isArray(cursor[node]) && !Array.isArray(data) && !dataIsNull) {
+                            cursor[node].push(data);
+                        } else {
+                            cursor[node] = dataIsNull ? undefined : data;
+                        }
+                    }
+                } else {
+                    cursor = cursor[node];
+                }
+            }
+            cache.set(name, currentData, ttl === "" ? 0 : ttl);
+        } catch (error) {
+            logger.debug(`error setting data to custom variable ${name} using property path ${propertyPath}`);
+        }
+    }
 };
 
-exports.getCustomVariable = (name) => {
-    return cache.get(name);
+exports.getCustomVariable = (name, propertyPath = null) => {
+    let data = cache.get(name);
+
+    if (!data) {
+        return null;
+    }
+
+    if (!propertyPath) {
+        return data;
+    }
+
+    try {
+        let pathNodes = propertyPath.split(".");
+        for (let i = 0; i < pathNodes.length; i++) {
+            let node = pathNodes[i];
+            // parse to int for array access
+            if (!isNaN(node)) {
+                node = parseInt(node);
+            }
+            data = data[node];
+        }
+        return data;
+    } catch (error) {
+        logger.debug(`error getting data from custom variable ${name} using property path ${propertyPath}`);
+        return null;
+    }
 };
 
 
--- backend/variables/builtin/custom-variable.js
+++ backend/variables/builtin/custom-variable.js
@@ -4,17 +4,25 @@ const customVariableManager = require("../../common/custom-variable-manager");
 
 const { OutputDataType } = require("../../../shared/variable-contants");
 
+function isObjectOrArray(data) {
+    return Array.isArray(data) || (typeof data === 'object' && !(typeof data === 'string' || data instanceof String));
+}
+
 const model = {
     definition: {
         handle: "customVariable",
         usage: "customVariable[name]",
-        description: "Get the data saved in the custom variable.",
+        description: "Get the data saved in the custom variable. Optionally provide a property path (using dot notation) or array index as a second argument.",
         possibleDataOutput: [OutputDataType.NUMBER, OutputDataType.TEXT]
     },
-    evaluator: (_, name) => {
-        let data = customVariableManager.getCustomVariable(name);
-        return data ? data : "[Can't Find Variable]";
+    evaluator: (_, name, propertyPath) => {
+        let data = customVariableManager.getCustomVariable(name, propertyPath);
+        if (data && isObjectOrArray(data)) {
+            data = JSON.stringify(data);
+        }
+        return data || "null";
     }
 };
+
 
 module.exports = model;
