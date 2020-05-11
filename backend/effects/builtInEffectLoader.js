07eaeee76f767ab8e2a1e56989eacd7adb0b8b26
Firebottle <mage@gamingobscura.com>
2020-05-11 10:14:11 -0500

    Add new active list management effect. Remove unused functions.

--- backend/effects/builtInEffectLoader.js
+++ backend/effects/builtInEffectLoader.js
@@ -40,6 +40,7 @@ exports.loadEffects = () => {
     const stopEffectExecution = require("./builtin/stop-effect-execution");
     const adBreak = require("./builtin/ad-break");
     const runProgram = require("./builtin/run-program");
+    const activeUserLists = require("./builtin/activeUserLists");
 
     // register them
     effectManager.registerEffect(playSoundEffect);
@@ -78,4 +79,5 @@ exports.loadEffects = () => {
     effectManager.registerEffect(stopEffectExecution);
     effectManager.registerEffect(adBreak);
     effectManager.registerEffect(runProgram);
+    effectManager.registerEffect(activeUserLists);
 };
