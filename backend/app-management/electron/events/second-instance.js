68d1155ecca6197ea755c92323b5275c42156785
Erik Bigler <erikbigler@gmail.com>
2020-06-15 03:17:05 -0500

    correct require path

--- backend/app-management/electron/events/second-instance.js
+++ backend/app-management/electron/events/second-instance.js
@@ -2,7 +2,7 @@
 
 exports.secondInstance = () => {
     // Someone tried to run a second instance, we should focus our window.
-    const { mainWindow } = require("./backend/app-management/electron/window-management");
+    const { mainWindow } = require("../window-management");
     if (mainWindow) {
         if (mainWindow.isMinimized()) {
             mainWindow.restore();
