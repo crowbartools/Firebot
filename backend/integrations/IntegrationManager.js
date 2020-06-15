89ef09ba506d7d930b8e6237be89b560c07264fc
Erik Bigler <erikbigler@gmail.com>
2020-06-15 15:50:46 -0500

    only log jsondb errors if they arent dataerrors

--- backend/integrations/IntegrationManager.js
+++ backend/integrations/IntegrationManager.js
@@ -17,8 +17,6 @@ class IntegrationManager extends EventEmitter {
     }
 
     registerIntegration(integration) {
-        // TODO: validate integration
-
         integration.definition.linked = false;
 
         if (integration.definition.linkType === "auth") {
@@ -38,7 +36,9 @@ class IntegrationManager extends EventEmitter {
                 integration.definition.linked = false;
             }
         } catch (err) {
-            logger.warn(err);
+            if (err.name !== "DataError") {
+                logger.warn(err);
+            }
         }
 
         integration.integration.init(
