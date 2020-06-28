ae6797fc6d8421ed650b546f04efed1549c0b551
TheLastMage <mage@gamingobscura.com>
2020-06-28 18:47:49 -0500

    Setup clips effect for twitch.

--- backend/common/handlers/createClipProcessor.js
+++ backend/common/handlers/createClipProcessor.js
@@ -1,41 +1,33 @@
 "use strict";
 
 const twitchChat = require("../../chat/twitch-chat");
-const mixerApi = require("../../mixer-api/api");
-const util = require("../../utility");
 const logger = require("../../logwrapper");
 const accountAccess = require("../account-access");
 const { settings } = require("../settings-access");
 const sanitize = require("sanitize-filename");
-const moment = require("moment");
 const path = require("path");
 const discordEmbedBuilder = require("../../integrations/builtin/discord/discord-embed-builder");
 const discord = require("../../integrations/builtin/discord/discord-message-sender");
 const downloadClip = require("./clip-downloader");
 
-function downloadAndSaveClip(uri, title = "") {
-    return new Promise((resolve, reject) => {
-        logger.info("Starting clip download...");
+const twitchApi = require("../../twitch-api/client");
+const client = twitchApi.getClient();
 
+function downloadAndSaveClip(clipProperties) {
+    return new Promise((resolve, reject) => {
+        logger.debug("Starting clip download...");
+
+        const uri = clipProperties.url;
         if (uri == null || uri === "") {
             return reject("Invalid uri");
         }
 
-        let timestamp = moment().format("DD-MM-YY (HHmmss)");
-
-        // sanitize title so its a valid filename and add timestamp to avoid name conflicts
-        title = sanitize(`${title} ${timestamp}`);
-
-        if (title === "") {
-            return reject("Invalid title");
-        }
-
-        let clipsDownloadFolder = settings.getClipDownloadFolder();
-
-        let fullPath = path.join(clipsDownloadFolder, title + ".mp4");
+        const title = sanitize(`${clipProperties.title}-${clipProperties.creationDate}`);
+        const clipsDownloadFolder = settings.getClipDownloadFolder();
+        const fullPath = path.join(clipsDownloadFolder, title + ".mp4");
 
         downloadClip(uri, fullPath).then(() => {
-            logger.info("Successfully completed clip download!");
+            logger.debug("Successfully completed clip download!");
             resolve();
         },
         (e) => {
@@ -48,29 +40,17 @@ function downloadAndSaveClip(uri, title = "") {
 exports.createClip = async function(effect, trigger) {
 
     const streamerAccount = accountAccess.getAccounts().streamer;
-    const broadcast = await mixerApi.channels.getStreamersBroadcast();
-
-    let streamerData = accountAccess.getAccounts().streamer;
-    if (!streamerData.partnered && !streamerData.canClip) {
-        logger.warn("An unapproved user type attempted to create a clip!");
-        renderWindow.webContents.send('error', `Failed to create a clip on Mixer. Reason: Not allowed to create clips at this time.`);
-        return false;
-    }
+    const broadcast = await client.helix.streams.getStreamByUserName(streamerAccount.username);
+    const channelId = (await client.helix.users.getUserByName(streamerAccount.username)).id;
 
     if (broadcast == null) {
-        renderWindow.webContents.send('error', `Failed to create a clip on Mixer. Reason: Streamer channel is not live.`);
+        renderWindow.webContents.send('error', `Failed to create a clip. Reason: Streamer is not live.`);
         return false;
     }
 
     if (effect.postLink) {
         twitchChat.sendChatMessage("Creating clip...");
     }
-
-    let title = effect.clipTitle;
-    if (title == null || title === "") {
-        title = "$streamTitle (Created by $user)";
-    }
-    title = await util.populateStringWithTriggerData(title, trigger);
 
     let duration = 30;
     if (effect.clipDuration != null) {
@@ -87,45 +67,53 @@ exports.createClip = async function(effect, trigger) {
     // enforce limits
     if (duration < 5) {
         duration = 5;
-    } else if (duration > 300) {
-        duration = 300;
+    } else if (duration > 60) {
+        duration = 60;
     }
 
-    const clipProperties = await mixerApi.clips.createClip({
-        broadcastId: broadcast.id,
-        clipDurationInSeconds: duration,
-        highlightTitle: title
+    const clipProperties = await client.helix.clips.createClip({
+        channelId: channelId,
+        createAfterDelay: true
     });
 
-    const creationSuccessful = clipProperties != null;
+    if (clipProperties == null) {
+        if (effect.postLink) {
+            twitchChat.sendChatMessage("Whoops! Something went wrong when creating a clip. :(");
+        }
+        renderWindow.webContents.send('error', `Failed to create a clip.`);
+        return;
+    }
+
+    const creationSuccessful = await client.helix.clips.getClipById(clipProperties);
 
     if (creationSuccessful) {
         if (effect.postLink) {
-            const message = `Clip created: https://mixer.com/${streamerAccount.username}?clip=${clipProperties.shareableId}`;
+            const message = `${creationSuccessful.embedUrl}`;
             twitchChat.sendChatMessage(message);
         }
 
+        /** Discord embed has not been converted for twitch yet 6/28/20
         if (effect.postInDiscord) {
             const clipEmbed = await discordEmbedBuilder.buildClipEmbed(clipProperties);
             discord.sendDiscordMessage(effect.discordChannelId, "A new clip was created!", clipEmbed);
         }
+         */
 
-        const streamLocator = clipProperties.contentLocators.find(l => l.locatorType === "HlsStreaming");
-        if (streamLocator != null) {
+        if (effect.download) {
             try {
-                await downloadAndSaveClip(streamLocator.uri, title);
+                await downloadAndSaveClip(creationSuccessful);
                 renderWindow.webContents.send('eventlog', {type: "general", username: "System:", event: `Successfully saved clip to download folder.`});
             } catch (e) {
                 renderWindow.webContents.send('eventlog', {type: "general", username: "System:", event: `Failed to download and save clip for reason: ${e}`});
             }
         }
 
-        logger.info("Successfully created a Mixer clip!");
+        logger.info("Successfully created a clip!");
     } else {
         if (effect.postLink) {
             twitchChat.sendChatMessage("Whoops! Something went wrong when creating a clip. :(");
         }
-        renderWindow.webContents.send('error', `Failed to create a clip on Mixer`);
+        renderWindow.webContents.send('error', `Failed to create a clip.`);
     }
     return true;
 };
