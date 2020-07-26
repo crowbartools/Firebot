fcc58c67c2d09b51e02bbc8f83a8dc8cf1fc7faa
Erik Bigler <erikbigler@gmail.com>
2020-07-26 03:26:12 -0500

    fix active chatters

--- backend/chat/chat-listeners/active-user-handler.js
+++ backend/chat/chat-listeners/active-user-handler.js
@@ -6,6 +6,8 @@ const NodeCache = require("node-cache");
 const ACTIVE_TIMEOUT = 300; // 5 mins
 const activeUsers = new NodeCache({ stdTTL: ACTIVE_TIMEOUT, checkperiod: 15 });
 
+const activeChattersRoleHandler = require("../../roles/role-managers/active-chatters");
+
 /**
  * Add or update an active user
  * @arg {import('twitch-chat-client/lib/ChatUser').default} chatUser
@@ -40,6 +42,8 @@ exports.addActiveUser = async (chatUser) => {
         // user is still active reset ttl
         activeUsers.ttl(chatUser.userId, ACTIVE_TIMEOUT);
     }
+
+    await activeChattersRoleHandler.addOrUpdateActiveChatter(chatUser.userId, chatUser.displayName);
 
     await userDatabase.incrementDbField(chatUser.userId, "chatMessages");
 };
