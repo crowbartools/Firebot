e1c55de18c3dfd7ac602dbb7e0feee001d0bea77
Erik Bigler <erikbigler@gmail.com>
2020-10-12 23:47:37 -0500

    tweak secrets manager

--- backend/secrets-manager.js
+++ backend/secrets-manager.js
@@ -12,32 +12,37 @@ const logger = require("./logwrapper");
  * @property {string} streamLabsClientSecret
  */
 
+/**@type {(keyof FirebotSecrets)[]} */
+const expectedKeys = [
+    "twitchClientId",
+    "twitchClientSecret",
+    "tipeeeStreamClientId",
+    "tipeeeStreamClientSecret",
+    "streamLabsClientId",
+    "streamLabsClientSecret"
+];
+
 exports.testSecrets = () => {
+    let missingKeys = expectedKeys;
     try {
         /**@type {FirebotSecrets} */
         const secrets = require("../secrets.json") || {};
 
-        /**@type {(keyof FirebotSecrets)[]} */
-        const expected = [
-            "twitchClientId",
-            "twitchClientSecret",
-            "tipeeeStreamClientId",
-            "tipeeeStreamClientSecret",
-            "streamLabsClientId",
-            "streamLabsClientSecret"
-        ];
-        const missing = expected.filter(k => secrets[k] == null);
+        missingKeys = expectedKeys.filter(k => secrets[k] == null);
 
-        if (missing.length) {
-            logger.error(`secrets.json is missing the following keys: ${missing.join(", ")}`);
+        if (!missingKeys.length) {
+            // We have no missing keys
+            return true;
+        }
+    } catch (err) {
+        if (err && err.code === "MODULE_NOT_FOUND") {
+            logger.error("Unable to find secrets.json in the root directory. Please create it.");
             return false;
         }
-
-        return true;
-    } catch (err) {
-        logger.error("Unable to find secrets.json in the root directory. Please create it.");
-        return false;
     }
+
+    logger.error(`secrets.json is missing the following keys: ${missingKeys.join(", ")}`);
+    return false;
 };
 
 /**@type {FirebotSecrets} */
