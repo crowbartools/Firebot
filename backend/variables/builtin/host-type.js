d3babe62f5f20d0ef665f012526b45f5bc629789
Erik Bigler <erikbigler@gmail.com>
2021-03-22 00:38:53 -0500

    Add $hostType var #1039

---
+++ backend/variables/builtin/host-type.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const {
+    EffectTrigger
+} = require("../../effects/models/effectModels");
+
+const { OutputDataType, VariableCategory } = require("../../../shared/variable-contants");
+
+let triggers = {};
+triggers[EffectTrigger.EVENT] = ["twitch:host"];
+triggers[EffectTrigger.MANUAL] = true;
+
+
+const model = {
+    definition: {
+        handle: "hostType",
+        description: "Get the type of host ('manual' or 'auto')",
+        triggers: triggers,
+        categories: [VariableCategory.COMMON],
+        possibleDataOutput: [OutputDataType.TEXT]
+    },
+    evaluator: async (trigger) => {
+        return trigger.metadata.eventData.auto ? 'auto' : 'manual';
+    }
+};
+
+module.exports = model;
