4b6f51114f8f6f0f01b0c092c5107f02a96a7e1e
CaveMobster <cavemobster@gmail.com>
2021-04-19 12:12:13 +0200

    Update the team data retrieval for the viewer details

--- backend/common/user-access.js
+++ backend/common/user-access.js
@@ -83,7 +83,7 @@ async function getUserDetails(userId) {
     }
 
     const userRoles = await twitchApi.users.getUsersChatRoles(twitchUser.id);
-    const teamRoles = await twitchApi.teams.getMatchingTeams(twitchUser.id, streamerData.userId);
+    const teamRoles = await twitchApi.teams.getMatchingTeamsById(twitchUser.id);
 
     const userFollowsStreamerResponse = await client.helix.users.getFollows({
         user: userId,
