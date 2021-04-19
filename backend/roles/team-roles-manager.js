09f40af7df5a6acc5bb42f9dd40b37f617e2fdba
CaveMobster <cavemobster@gmail.com>
2021-04-19 12:34:32 +0200

    Update the team roles manager to get its data from the teams api file

--- backend/roles/team-roles-manager.js
+++ backend/roles/team-roles-manager.js
@@ -3,42 +3,9 @@
 const accountAccess = require("../../common/account-access");
 const twitchApi = require("../client");
 
-async function getTeams(broadcasterId) {
-    const client = twitchApi.getClient();
-    const teams = await client.kraken.channels.getChannelTeams(broadcasterId);
-
-    if (teams == null) {
-        return null;
-    }
-
-    return teams;
-}
-
-async function getMatchingTeams(userId, streamerId) {
-    const userTeams = await getTeams(userId);
-    const streamerTeams = await getTeams(streamerId);
-
-    if (userTeams == null || streamerTeams == null) {
-        return null;
-    }
-
-    const teams = [];
-    for (let streamerTeam of streamerTeams) {
-        for (let userTeam of userTeams) {
-            if (streamerTeam.id === userTeam.id) {
-                teams.push(streamerTeam);
-            }
-        }
-    }
-
-    return teams;
-}
-
 async function getAllTeamRolesForViewer(username) {
     const client = twitchApi.getClient();
-    const streamer = accountAccess.getAccounts().streamer;
-    const user = await client.helix.users.getUserByName(username);
-    const roles = await getMatchingTeams(user.id, streamer.userId);
+    const roles = await twitchApi.teams.getMatchingTeamsByUsername(username);
     
     return roles
         .map(role => {
