fc4f45c62b32dea8be6589fe2c7375ce082240ea
CaveMobster <cavemobster@gmail.com>
2021-04-19 23:27:47 +0200

    Update the team roles manager to include mapping and an event listener

--- backend/roles/team-roles-manager.js
+++ backend/roles/team-roles-manager.js
@@ -1,18 +1,44 @@
 "use strict";
 
-const twitchApi = require("../client");
+const twitchApi = require("../twitch-api/api");
+const frontendCommunicator = require("../common/frontend-communicator");
 
-async function getAllTeamRolesForViewer(username) {
-    const client = twitchApi.getClient();
-    const roles = await client.teams.getMatchingTeamsByUsername(username);
-    
-    return roles
-        .map(role => {
-            return {
-                id: role.id,
-                name: role.displayName
-            };
-        });
+function mapRoles(teams) {
+    return teams
+    .map(team => {
+        return {
+            id: team.id,
+            name: team.displayName
+        };
+    });
 }
 
+async function getAllTeamRolesForViewer(username) {
+    const roles = await twitchApi.teams.getMatchingTeamsByUsername(username);
+    
+    return mapRoles(roles);
+}
+
+async function getTeamRoles() {
+    const teams = await twitchApi.teams.getStreamerTeams();
+
+    if (teams == null) {
+        return null;
+    }
+
+    return mapRoles(teams);
+}
+
+frontendCommunicator.onAsync("getTeamRoles", async () => {
+    const roles = await getTeamRoles();
+
+    if (roles == null) {
+        return null;
+    }
+
+    return roles;
+    
+});
+
+exports.getTeamRoles = getTeamRoles;
 exports.getAllTeamRolesForViewer = getAllTeamRolesForViewer;
