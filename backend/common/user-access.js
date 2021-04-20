4288e8f232408b06d882c63c8a893f81648fa005
CaveMobster <cavemobster@gmail.com>
2021-04-20 18:09:46 +0200

    Add subscriber role data to the user access

--- backend/common/user-access.js
+++ backend/common/user-access.js
@@ -84,6 +84,7 @@ async function getUserDetails(userId) {
 
     const userRoles = await twitchApi.users.getUsersChatRoles(twitchUser.id);
     const teamRoles = await twitchApi.teams.getMatchingTeamsById(twitchUser.id);
+    const subscriberRole = await twitchApi.users.getUserSubscriberRole(twitchUser.id);
 
     const userFollowsStreamerResponse = await client.helix.users.getFollows({
         user: userId,
@@ -106,6 +107,7 @@ async function getUserDetails(userId) {
         twitchUserData.isBanned = isBanned;
         twitchUserData.userRoles = userRoles || [];
         twitchUserData.teamRoles = teamRoles || [];
+        twitchUserData.subscriberRole = subscriberRole || null;
     }
 
     const userDetails = {
