1e5ca794e6dea8bc190d469f8dcc90d51bfb3997
CaveMobster <cavemobster@gmail.com>
2021-04-20 16:09:43 +0200

    Add constants for sub roles

---
+++ shared/subscriber-roles.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const subRoles = [
+    {
+        id: "prime",
+        name: "Prime"
+    },
+    {
+        id: "tier1",
+        name: "Tier 1"
+    },
+    {
+        id: "tier2",
+        name: "Tier 2"
+    },
+    {
+        id: "tier3",
+        name: "Tier 3"
+    }
+];
+
+exports.getSubscriberRoles = () => subRoles;
+
