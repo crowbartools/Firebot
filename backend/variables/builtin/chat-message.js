aeefbfa23baa543b5d042b95183132c7920297d6
TheLastMage <mage@gamingobscura.com>
2021-04-27 08:06:11 -0500

    Update variable.

--- backend/variables/builtin/chat-message.js
+++ backend/variables/builtin/chat-message.js
@@ -31,7 +31,7 @@ const model = {
 
         } else if (trigger.type === EffectTrigger.EVENT) {
             // if trigger is event, build chat message from chat event data
-            chatMessage = trigger.metadata.chatMessage || trigger.metadata.eventData.messageText;
+            chatMessage = trigger.metadata.chatMessage || trigger.metadata.eventData.chatMessage;
         }
 
         return chatMessage.trim();
