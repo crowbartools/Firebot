1d0b4cee19b5c978fa78490824341ab2813454ed
CaveMobster <cavemobster@gmail.com>
2021-04-29 12:16:08 +0200

    Add whitespace

--- gui/app/controllers/eventsController.js
+++ gui/app/controllers/eventsController.js
@@ -17,6 +17,7 @@
                 if (eventsService.getEventGroup(selectedTab) == null) {
                     return eventsService.getMainEvents();
                 }
+                
                 return eventsService.getEventGroup(selectedTab).events;
             };
 
