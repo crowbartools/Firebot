039f791f697a4f10c14940a4222be03c1065a1ee
CaveMobster <cavemobster@gmail.com>
2021-05-07 22:23:49 +0200

    Fix linting issues

--- backend/twitch-api/pubsub/pubsub-client.js
+++ backend/twitch-api/pubsub/pubsub-client.js
@@ -86,9 +86,9 @@ async function createClient() {
         listeners.push(redemptionListener);
 
         const whisperListener = await pubSubClient.onWhisper(streamer.userId, (message) => {
-                const whisperListener = require("../../events/twitch-events/whisper");
-                whisperListener.triggerWhisper(message.senderName, message.text);
-            });
+            const whisperListener = require("../../events/twitch-events/whisper");
+            whisperListener.triggerWhisper(message.senderName, message.text);
+        });
         listeners.push(whisperListener);
 
     } catch (err) {
