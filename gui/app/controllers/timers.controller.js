243ec5506e78f66476092b7a2d3907dfb243a946
CaveMobster <cavemobster@gmail.com>
2021-07-17 15:38:48 +0200

    Add headers and options for the timers item table

--- gui/app/controllers/timers.controller.js
+++ gui/app/controllers/timers.controller.js
@@ -20,68 +20,79 @@
                 timerService.saveTimer(timer);
             };
 
-            $scope.deleteTimer = timer => {
-                timerService.deleteTimer(timer);
-            };
-
-            $scope.openAddOrEditTimerModal = function(timer) {
-                utilityService.showModal({
-                    component: "addOrEditTimerModal",
-                    resolveObj: {
-                        timer: () => timer
-                    },
-                    closeCallback: resp => {
-                        let action = resp.action,
-                            timer = resp.timer;
-
-                        switch (action) {
-                        case "add":
-                        case "update":
-                            timerService.saveTimer(timer);
-                            break;
-                        case "delete":
-                            timerService.deleteTimer(timer);
-                            break;
-                        }
-                    }
-                });
-            };
-
             $scope.duplicateTimer = timer => {
                 let copiedTimer = objectCopyHelper.copyObject("timer", timer);
                 copiedTimer.name += " copy";
                 timerService.saveTimer(copiedTimer);
             };
 
-            $scope.timerMenuOptions = [
+            $scope.onTimersUpdated = (timers) => {
+                timerService.saveAllTimers(timers);
+            };
+
+            $scope.headers = [
                 {
-                    html: `<a href ><i class="far fa-pen" style="margin-right: 10px;"></i> Edit</a>`,
-                    click: function ($itemScope) {
-                        let timer = $itemScope.timer;
-                        $scope.openAddOrEditTimerModal(timer);
-                    }
+                    name: "NAME",
+                    icon: "fa-user",
+                    cellTemplate: `{{data.name}}`,
+                    cellController: () => {}
                 },
                 {
-                    html: `<a href ><i class="far fa-toggle-off" style="margin-right: 10px;"></i> Toggle Enabled</a>`,
-                    click: function ($itemScope) {
-                        let timer = $itemScope.timer;
-                        $scope.toggleTimerActiveState(timer);
-                    }
+                    name: "INTERVAL",
+                    icon: "fa-stopwatch",
+                    cellTemplate: `{{data.interval}}`,
+                    cellController: () => {}
                 },
                 {
-                    html: `<a href><i class="far fa-clone" style="margin-right: 10px;"></i> Duplicate</a>`,
-                    click: function ($itemScope) {
-                        let timer = $itemScope.timer;
-                        $scope.duplicateTimer(timer);
-                    }
-                },
-                {
-                    html: `<a href style="color: #fb7373;"><i class="far fa-trash-alt" style="margin-right: 10px;"></i> Delete</a>`,
-                    click: function ($itemScope) {
-                        let timer = $itemScope.timer;
-                        $scope.deleteTimer(timer);
-                    }
+                    name: "EFFECTS",
+                    icon: "fa-magic",
+                    cellTemplate: `{{data.effects ? data.effects.list.length : 0}}`,
+                    cellControler: () => {}
                 }
             ];
+
+            $scope.timerOptions = (item) => {
+                const options = [
+                    {
+                        html: `<a href ><i class="far fa-pen" style="margin-right: 10px;"></i> Edit</a>`,
+                        click: function () {
+                            $scope.openAddOrEditTimerModal(item);
+                        }
+                    },
+                    {
+                        html: `<a href ><i class="far fa-toggle-off" style="margin-right: 10px;"></i> Toggle Enabled</a>`,
+                        click: function ($itemScope) {
+                            let timer = $itemScope.timer;
+                            $scope.toggleTimerActiveState(timer);
+                        }
+                    },
+                    {
+                        html: `<a href ><i class="far fa-clone" style="margin-right: 10px;"></i> Duplicate</a>`,
+                        click: function () {
+                            $scope.duplicateTimer(item);
+                        }
+                    },
+                    {
+                        html: `<a href style="color: #fb7373;"><i class="far fa-trash-alt" style="margin-right: 10px;"></i> Delete</a>`,
+                        click: function () {
+                            utilityService
+                                .showConfirmationModal({
+                                    title: "Delete Timer",
+                                    question: `Are you sure you want to delete the Timer "${item.name}"?`,
+                                    confirmLabel: "Delete",
+                                    confirmBtnType: "btn-danger"
+                                })
+                                .then(confirmed => {
+                                    if (confirmed) {
+                                        timerService.deleteTimer(item);
+                                    }
+                                });
+
+                        }
+                    }
+                ];
+
+                return options;
+            };
         });
 }());
