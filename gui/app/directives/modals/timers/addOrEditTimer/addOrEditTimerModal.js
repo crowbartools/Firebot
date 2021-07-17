e2220f04ff38acb4871bbb4ba32d17a69d825c65
CaveMobster <cavemobster@gmail.com>
2021-07-17 17:03:23 +0200

    Update the add or edit timer modal

--- gui/app/directives/modals/timers/addOrEditTimer/addOrEditTimerModal.js
+++ gui/app/directives/modals/timers/addOrEditTimer/addOrEditTimerModal.js
@@ -4,14 +4,60 @@
 
 (function() {
     angular.module("firebotApp").component("addOrEditTimerModal", {
-        templateUrl: "./directives/modals/timers/addOrEditTimer/addOrEditTimerModal.html",
+        template: `
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" ng-click="$ctrl.dismiss()">&times;</span></button>
+                <h4 class="modal-title" id="newCooldownGroupLabel">{{$ctrl.isNewTimer ? 'Add New Timer' : 'Edit Timer - ' + $ctrl.timer.name}}</h4>
+            </div>
+            <div class="modal-body">
+                <div class="general-button-settings">
+                    <div class="settings-title">
+                        <h3>General Settings</h3>
+                    </div>
+                    <div class="input-group settings-commandGroup-groupName">
+                        <span class="input-group-addon">Name</span>
+                        <input type="text" class="form-control" ng-model="$ctrl.timer.name">
+                    </div>
+                    <div class="input-group settings-commandGroup-timer">
+                        <span class="input-group-addon">Interval(secs)</span>
+                        <input type="number" class="form-control" ng-model="$ctrl.timer.interval" placeholder="Seconds">
+                    </div>
+                    <div class="input-group settings-commandGroup-timer">
+                        <span class="input-group-addon">Required Chat Lines <tooltip text="'The minimum number of chat lines since the last interval.'"></tooltip></span>
+                        <input type="number" class="form-control" ng-model="$ctrl.timer.requiredChatLines" placeholder="">
+                    </div>
+                    <div class="controls-fb-inline">
+                        <label class="control-fb control--checkbox" ng-hide="$ctrl.isNewTimer">Enabled
+                            <input type="checkbox" ng-model="$ctrl.timer.active" aria-label="...">
+                            <div class="control__indicator"></div>
+                        </label>
+                        <label class="control-fb control--checkbox">Only Run When Live <tooltip text="'Uncheck this if you want this timer to run effects even when you are not live.'"></tooltip>
+                            <input type="checkbox" ng-model="$ctrl.timer.onlyWhenLive" aria-label="...">
+                            <div class="control__indicator"></div>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="function-button-settings" style="margin-top: 15px;">
+                    <effect-list header="What should this timer do?" effects="$ctrl.timer.effects" trigger="timer" trigger-meta="$ctrl.triggerMeta" update="$ctrl.effectListUpdated(effects)" modalId="{{$ctrl.modalId}}"></effect-list>
+                </div>
+                <p class="muted" style="font-size:11px;margin-top:6px;">
+                    <b>ProTip:</b> If you want to have this timer display a single chat message at a time, try the <b>Run Random Effect</b> or <b>Run Sequential Effect</b>
+                </p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-link" ng-click="$ctrl.dismiss()">Cancel</button>
+                <button type="button" class="btn btn-primary" ng-click="$ctrl.save()">Save</button>
+            </div>
+        `,
         bindings: {
             resolve: "<",
             close: "&",
             dismiss: "&",
             modalInstance: "<"
         },
-        controller: function($scope, utilityService, ngToast) {
+        controller: function($scope, utilityService, ngToast, timerService) {
             let $ctrl = this;
 
             $ctrl.timer = {
@@ -53,12 +99,7 @@
                 $ctrl.timer.effects = effects;
             };
 
-            $ctrl.delete = function() {
-                if ($ctrl.timer == null) return;
-                $ctrl.close({ $value: { timer: $ctrl.timer, action: "delete" } });
-            };
-
-            function validateTimer() {
+            function timerValid() {
                 if ($ctrl.timer.name === "") {
                     ngToast.create("Please provide a name for the Timer.");
                     return false;
@@ -70,13 +111,17 @@
             }
 
             $ctrl.save = function() {
-                if (!validateTimer()) return;
+                if (!timerValid()) return;
 
-                let action = $ctrl.isNewTimer ? "add" : "update";
-                $ctrl.close({
-                    $value: {
-                        timer: $ctrl.timer,
-                        action: action
+                timerService.saveTimer($ctrl.timer).then(successful => {
+                    if (successful) {
+                        $ctrl.close({
+                            $value: {
+                                timer: $ctrl.timer
+                            }
+                        });
+                    } else {
+                        ngToast.create("Failed to save timer. Please try again or view logs for details.");
                     }
                 });
             };
