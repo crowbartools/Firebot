06ad2fa5438125422198cbae3c88a929242452ce
CaveMobster <cavemobster@gmail.com>
2021-08-19 10:06:14 +0200

    Add mixer roles back

--- shared/mixer-roles.js
+++ shared/mixer-roles.js
@@ -1,5 +1,36 @@
 "use strict";
 
+const mixerRoles = [
+    {
+        id: "Pro",
+        name: "Pro Users"
+    },
+    {
+        id: "Subscriber",
+        name: "Subscribers"
+    },
+    {
+        id: "Partner",
+        name: "Partners"
+    },
+    {
+        id: "Mod",
+        name: "Moderators"
+    },
+    {
+        id: "ChannelEditor",
+        name: "Channel Editors"
+    },
+    {
+        id: "Staff",
+        name: "Staff"
+    },
+    {
+        id: "Owner",
+        name: "Streamer"
+    }
+];
+
 // get a firebot mixer role object baed on the raw role id from mixers api
 function mapMixerRole(rawRole) {
     let mappedRole;
