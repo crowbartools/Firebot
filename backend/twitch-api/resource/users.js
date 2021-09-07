e79e9adb9cce33ca0740af61f94a721410cd3c17
CaveMobster <cavemobster@gmail.com>
2021-09-07 13:26:46 +0200

    Remove the chatinfo function

--- backend/twitch-api/resource/users.js
+++ backend/twitch-api/resource/users.js
@@ -1,32 +1,7 @@
 "use strict";
 
-const accountAccess = require("../../common/account-access");
 const twitchApi = require("../client");
-const { TwitchAPICallType } = require("twitch/lib");
 const logger = require("../../logwrapper");
-
-const getUserChatInfo = async (userId) => {
-    const client = twitchApi.getClient();
-
-    const streamer = accountAccess.getAccounts().streamer;
-
-    const chatUser = await client.callApi({
-        type: TwitchAPICallType.Kraken,
-        url: `users/${userId}/chat/channels/${streamer.userId}`
-    });
-
-    return chatUser;
-};
-
-const getUserChatInfoByName = async (username) => {
-    try {
-        const client = twitchApi.getClient();
-        const user = await client.helix.users.getUserByName(username);
-        return getUserChatInfo(user.id);
-    } catch (error) {
-        logger.error("Couldn't get user chat info by name", error);
-    }
-};
 
 const blockUserByName = async (username) => {
     try {
@@ -48,6 +23,5 @@ const unblockUserByName = async (username) => {
     }
 };
 
-exports.getUserChatInfoByName = getUserChatInfoByName;
 exports.blockUserByName = blockUserByName;
 exports.unblockUserByName = unblockUserByName;
