e30ca678670a53feb86684fc824976b461ff13a8
CaveMobster <cavemobster@gmail.com>
2021-10-08 16:46:45 +0200

    Refactor the builtin restrictions loader and file names

--- backend/restrictions/builtin-restrictions-loader.js
+++ backend/restrictions/builtin-restrictions-loader.js
@@ -2,24 +2,19 @@
 
 const restrictionsManager = require("./restriction-manager");
 
-exports.loadRestrictions = function() {
-    const permissions = require("./builtin/permissions");
-    const channelCurrency = require("./builtin/channelCurrency");
-    const activeChatUsers = require("./builtin/activeChatUsers");
-    const customVariable = require('./builtin/customVariable');
-    const viewTime = require('./builtin/viewTimeRestriction');
-    const chatMessages = require('./builtin/chatMessages');
-    const followCheck = require('./builtin/followCheck');
-    const channelViewers = require('./builtin/channelViewers');
-    const channelGame = require('./builtin/channelGame');
-
-    restrictionsManager.registerRestriction(permissions);
-    restrictionsManager.registerRestriction(channelCurrency);
-    restrictionsManager.registerRestriction(activeChatUsers);
-    restrictionsManager.registerRestriction(customVariable);
-    restrictionsManager.registerRestriction(viewTime);
-    restrictionsManager.registerRestriction(chatMessages);
-    restrictionsManager.registerRestriction(followCheck);
-    restrictionsManager.registerRestriction(channelViewers);
-    restrictionsManager.registerRestriction(channelGame);
+exports.loadRestrictions = () => {
+    [
+        'active-chat-users',
+        'channel-currency',
+        'channel-game',
+        'chat-messages',
+        'custom-variable',
+        'follow-check',
+        'permissions',
+        'view-time',
+        'viewer-count'
+    ].forEach(filename => {
+        let definition = require(`./builtin/${filename}.js`);
+        restrictionsManager.registerRestriction(definition);
+    });
 };
