d322827e439196de597933d6b87409fa82986dd9
Erik Bigler <erikbigler@gmail.com>
2021-12-01 12:10:24 -0600

    fix manage fonts opening overlay modal instead

--- gui/app/directives/settings/categories/overlay-settings.js
+++ gui/app/directives/settings/categories/overlay-settings.js
@@ -43,7 +43,7 @@
                     >
                         <firebot-button 
                             text="Manage Fonts"
-                            ng-click="settings.showOverlayInfoModal()"
+                            ng-click="showFontManagementModal()"
                         />
                     </firebot-setting>
 
