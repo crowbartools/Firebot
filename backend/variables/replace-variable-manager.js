1eb1cee991c60dd0cc7b4000c3edae2206005525
SReject <SReject@users.noreply.github.com>
2021-12-14 11:26:20 -0500

    Comment

--- backend/variables/replace-variable-manager.js
+++ backend/variables/replace-variable-manager.js
@@ -33,6 +33,7 @@ class ReplaceVariableManager extends EventEmitter {
     }
 
     getReplaceVariables () {
+        // Map register variables Map to array
         let registeredVariables = this._registeredVariableHandlers;
         let variables = [];
         /* eslint-disable-next-line no-unused-vars */
