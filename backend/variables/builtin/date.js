af0c0eb4f8352e3f2fbc98c85906bb95ca7ad6fe
CaveMobster <cavemobster@gmail.com>
2021-12-23 17:37:54 +0100

    Add an option to subtract time from the date variable

--- backend/variables/builtin/date.js
+++ backend/variables/builtin/date.js
@@ -17,17 +17,25 @@ const model = {
             {
                 usage: "date[MMM Do YYY, 2, days]",
                 description: "Adds 2 days to the current date (or use other units i.e. months, years, etc.)."
+            },
+            {
+                usage: "date[MMM Do YYY, -2, days]",
+                description: "Subtract 2 days from the current date (or use other units i.e. months, years, etc.)."
             }
         ],
         description: "The current date formatted as MMM Do YYYY",
         categories: [VariableCategory.COMMON],
         possibleDataOutput: [OutputDataType.TEXT]
     },
-    evaluator: (_, template = 'MMM Do YYYY', steps, key) => {
+    evaluator: (_, template = 'MMM Do YYYY', steps = 0, key) => {
         let now = moment();
 
         if (steps > 0 && key !== null) {
             now.add(steps, key);
+        }
+
+        if (steps < 0 && key !== null) {
+            now.subtract(Math.abs(steps), key);
         }
 
         return now.format(template.toString());
