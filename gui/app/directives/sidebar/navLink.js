ab4271e9861784aee66b858357f9c96648b704b7
CaveMobster <cavemobster@gmail.com>
2022-01-07 17:25:04 +0100

    Fix main menu accessibility

--- gui/app/directives/sidebar/navLink.js
+++ gui/app/directives/sidebar/navLink.js
@@ -10,18 +10,20 @@
             badgeText: "<"
         },
         template: `
-            <a draggable=false class="fb-nav-link" href="{{$ctrl.href}}" ng-class="{'selected': $ctrl.sbm.tabIsSelected($ctrl.page)}" ng-click="$ctrl.sbm.setTab($ctrl.page, $ctrl.name)"  uib-tooltip="{{!$ctrl.sbm.navExpanded ? $ctrl.name : ''}}" tooltip-placement="right" tooltip-append-to-body="true" role="menuitem">
-                <div class="nav-link-bar"></div>
-                <div class="nav-link-icon">
-                  <span class="nav-icon-wrapper">
-                    <i ng-class="$ctrl.getClass()"></i>
-                  </span>
-                </div>
-                <div class="nav-link-title" ng-class="{'contracted': !$ctrl.sbm.navExpanded}">{{$ctrl.name}}</div>
-                <div ng-show="$ctrl.hasBadge" class="nav-update-badge" ng-class="{'contracted': !$ctrl.sbm.navExpanded}">
-                    <span class="label label-danger">{{$ctrl.badgeText}}</span>
-                </div>
-            </a>
+            <li>
+                <a draggable=false class="fb-nav-link" href="{{$ctrl.href}}" ng-class="{'selected': $ctrl.sbm.tabIsSelected($ctrl.page)}" ng-click="$ctrl.sbm.setTab($ctrl.page, $ctrl.name)"  uib-tooltip="{{!$ctrl.sbm.navExpanded ? $ctrl.name : ''}}" tooltip-placement="right" tooltip-append-to-body="true">
+                    <div class="nav-link-bar"></div>
+                    <div class="nav-link-icon">
+                    <span class="nav-icon-wrapper">
+                        <i ng-class="$ctrl.getClass()"></i>
+                    </span>
+                    </div>
+                    <div class="nav-link-title" ng-class="{'contracted': !$ctrl.sbm.navExpanded}">{{$ctrl.name}}</div>
+                    <div ng-show="$ctrl.hasBadge" class="nav-update-badge" ng-class="{'contracted': !$ctrl.sbm.navExpanded}">
+                        <span class="label label-danger">{{$ctrl.badgeText}}</span>
+                    </div>
+                </a>
+            </li>
             `,
         controller: function(sidebarManager) {
             let ctrl = this;
