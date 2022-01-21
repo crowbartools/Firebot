fb94393e5bd9807b02b6e49a9f77d5245a624394
CaveMobster <cavemobster@gmail.com>
2022-01-21 22:37:55 +0100

    Update name references to the Streamlabs streaming software

--- backend/effects/builtin/play-video.js
+++ backend/effects/builtin/play-video.js
@@ -209,7 +209,7 @@ const playVideo = {
             <div class="effect-info alert alert-warning">
                 This effect requires the Firebot overlay to be loaded in your broadcasting software. <a href ng-click="showOverlayInfoModal(effect.overlayInstance)" style="text-decoration:underline">Learn more</a>
                 <br>
-                <strong>NOTE</strong>: Streamlabs OBS does not support mp4 videos in their browser source. If you have mp4 videos that you want to display in SLOBS, you will need to convert them to the <strong>.webm</strong> format.
+                <strong>NOTE</strong>: Streamlabs Desktop (formerly known as SLOBS) does not support mp4 videos in their browser source. If you have mp4 videos that you want to display in Streamlabs Desktop, you will need to convert them to the <strong>.webm</strong> format.
             </div>
         </eos-container>
     </div>
