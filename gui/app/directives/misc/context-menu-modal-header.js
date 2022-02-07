22353f1097d2108ffa1cc5ea7d7c359d95b0dee6
CaveMobster <cavemobster@gmail.com>
2022-02-07 13:03:46 +0100

    Fix sort tags not being to be removed with the option in the modal header

--- gui/app/directives/misc/context-menu-modal-header.js
+++ gui/app/directives/misc/context-menu-modal-header.js
@@ -6,7 +6,7 @@
             bindings: {
                 triggerType: "@",
                 triggerName: "<",
-                sortTags: "<",
+                sortTags: "=",
                 onClose: "&",
                 showTriggerName: "<"
             },
