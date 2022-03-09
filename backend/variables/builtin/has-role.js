efd966a4b481f7bf54690cac72b72d9976554ddc
SReject <SReject@users.noreply.github.com>
2022-03-09 11:33:15 -0500

    add $hasRole and $hasRoles variables

---
+++ backend/variables/builtin/has-role.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { EffectTrigger } = require("../../../shared/effect-constants");
+const { OutputDataType, VariableCategory } = require("../../../shared/variable-constants");
+
+const { viewerHasRolesByName } = require('../../roles/role-helpers');
+
+let triggers = {};
+triggers[EffectTrigger.COMMAND] = true;
+triggers[EffectTrigger.EVENT] = true;
+triggers[EffectTrigger.MANUAL] = true;
+triggers[EffectTrigger.CUSTOM_SCRIPT] = true;
+triggers[EffectTrigger.PRESET_LIST] = true;
+triggers[EffectTrigger.CHANNEL_REWARD] = true;
+
+module.exports = {
+    definition: {
+        handle: "hasRole",
+        usage: "hasRole[user, role]",
+        description: "Returns true if the user has the specified role. Only valid within $if",
+        triggers: triggers,
+        categories: [VariableCategory.COMMON, VariableCategory.USER],
+        possibleDataOutput: [OutputDataType.ALL]
+    },
+    evaluator: async (trigger, username, role) => {
+        if (username == null || username === '') {
+            return false;
+        }
+
+        if (role == null || role === '') {
+            return false;
+        }
+
+        return viewerHasRolesByName(username, [role]);
+    }
+};
---
+++ backend/variables/builtin/has-roles.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const { EffectTrigger } = require("../../../shared/effect-constants");
+const { OutputDataType, VariableCategory } = require("../../../shared/variable-constants");
+const { getAllRolesForViewer } = require('../../roles/role-helpers');
+
+let triggers = {};
+triggers[EffectTrigger.COMMAND] = true;
+triggers[EffectTrigger.EVENT] = true;
+triggers[EffectTrigger.MANUAL] = true;
+triggers[EffectTrigger.CUSTOM_SCRIPT] = true;
+triggers[EffectTrigger.PRESET_LIST] = true;
+triggers[EffectTrigger.CHANNEL_REWARD] = true;
+
+module.exports = {
+    definition: {
+        handle: "hasRoles",
+        usage: "hasRoles[user, any|all, role, rol2, ...]",
+        description: "Returns true if the user has the specified role. Only valid within $if",
+        examples: [
+            {
+                usage: 'hasRole[$user, any, mod, vip]',
+                description: "returns true if $user is a mod OR VIP"
+            },
+            {
+                usage: 'if[$user, all, mod, vip]',
+                description: "Returns true if $user is a mod AND a VIP"
+            }
+        ],
+        triggers: triggers,
+        categories: [VariableCategory.COMMON, VariableCategory.USER],
+        possibleDataOutput: [OutputDataType.ALL]
+    },
+    evaluator: async (trigger, username, respective, ...roles) => {
+        if (username == null || username === '') {
+            return false;
+        }
+
+        if (respective == null || respective === "") {
+            return false;
+        }
+
+        if (roles == null || roles.length === 0) {
+            return false;
+        }
+
+        respective = (respective + '').toLowerCase();
+        if (respective !== 'any' && respective !== 'all') {
+            return false;
+        }
+
+        const userRoles = await getAllRolesForViewer();
+
+        // any
+        if (respective === 'any') {
+            return userRoles.some(r => roles.includes(r.name));
+        }
+
+        // all
+        return roles.length === userRoles.filter(r => roles.includes(r.name)).length;
+    }
+};
