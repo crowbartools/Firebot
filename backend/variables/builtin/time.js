0b9bc620cf930fbb57b436fcd1b360c436a09da7
Zack Williamson <zwilliamson@outlook.com>
2022-05-18 11:31:46 -0400

    feat: add format helper text for $time variable

--- backend/variables/builtin/time.js
+++ backend/variables/builtin/time.js
@@ -11,11 +11,17 @@ const model = {
         handle: "time",
         description: "Outputs the current time.",
         categories: [VariableCategory.COMMON],
-        possibleDataOutput: [OutputDataType.TEXT]
+        possibleDataOutput: [OutputDataType.TEXT],
+        examples: [
+            {
+                usage: "time[format]",
+                description: 'Outputs the current time in a specific format. Format uses <a href="https://momentjs.com/docs/#/displaying/format/">moment.js</a> formatting rules.'
+            }
+        ]
     },
-    evaluator: (_, template = 'h:mm a') => {
+    evaluator: (_, format = 'h:mm a') => {
         const now = moment();
-        return now.format(template.toString());
+        return now.format(format.toString());
     }
 };
 
