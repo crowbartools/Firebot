d86886ba041610231c5fb59613bd42d7ad4d8026
Erik Bigler <erikbigler@gmail.com>
2022-08-08 09:07:44 -0500

    fix: fb-config warning

--- src/main/utils/fb-config.ts
+++ src/main/utils/fb-config.ts
@@ -2,6 +2,10 @@ import { JsonDB } from "node-json-db";
 import { Config } from "node-json-db/dist/lib/JsonDBConfig";
 import { getPathInFirebotData } from "../utils";
 
+type KeysMatching<T, V> = {
+    [K in keyof T]-?: T[K] extends V ? K : never;
+}[keyof T];
+
 export class FbConfig<Settings> {
     private defaultSettings: Settings;
     private settings: Settings;
@@ -12,16 +16,16 @@ export class FbConfig<Settings> {
         this.load(filePath);
     }
 
-    get<K extends keyof Settings>(key: K): Settings[K] {
+    get<K extends KeysMatching<Settings, string>>(key: K): Settings[K] {
         return this.settings[key] ?? this.defaultSettings[key];
     }
 
-    set<K extends keyof Settings, V extends Settings[K]>(
+    set<K extends KeysMatching<Settings, string>, V extends Settings[K]>(
         key: K,
         value: V
     ): void {
         this.settings[key] = value;
-        this.jsonDb.push(`/${key}`, value);
+        this.jsonDb.push(`/${key as string}`, value);
     }
 
     load(filePath: string): void {
