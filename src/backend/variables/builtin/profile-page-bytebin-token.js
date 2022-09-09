38a0c94478e9061d3e145745555d667e4e8a4733
Erik Bigler <erikbigler@gmail.com>
2022-09-09 10:31:23 -0500

    fix: $profilePageBytebinToken not functional (#1895)

--- src/backend/variables/builtin/profile-page-bytebin-token.js
+++ src/backend/variables/builtin/profile-page-bytebin-token.js
@@ -1,5 +1,3 @@
-// Migration: done
-
 "use strict";
 
 const cloudSync = require("../../cloud-sync/profile-sync");
@@ -8,17 +6,15 @@ const { OutputDataType } = require("../../../shared/variable-constants");
 const model = {
     definition: {
         handle: "profilePageBytebinToken",
-        description: "Get bytebin id to access allowed commands for a user. Access the json by going to https://bytebin.lucko.me/ID-HERE.",
+        description: "Get bytebin id for streamer profile. Access the json by going to https://bytebin.lucko.me/ID-HERE.",
         possibleDataOutput: [OutputDataType.TEXT]
     },
     evaluator: async (trigger, page = "commands") => {
-        const event = trigger.metadata.chatEvent;
-        const profileJSON = {
-            'username': event.chatEvent.user_name,
-            'userRoles': event.chatEvent.user_roles,
-            'profilePage': page
-        };
-        return await cloudSync.syncProfileData(profileJSON);
+        return await cloudSync.syncProfileData({
+            username: trigger.metadata.username,
+            userRoles: [],
+            profilePage: page
+        });
     }
 };
 
