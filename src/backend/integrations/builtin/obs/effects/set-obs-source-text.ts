40877776c1ddea29e770ff40b6efa588f4545667
Dennis <70665154+dennisrijsdijk@users.noreply.github.com>
2023-02-01 15:10:13 +0100

    fix: Enable use-text-area on relevant textboxes
    * fix: multiline inputs for OBS Text & File Write

--- src/backend/integrations/builtin/obs/effects/set-obs-source-text.ts
+++ src/backend/integrations/builtin/obs/effects/set-obs-source-text.ts
@@ -41,7 +41,7 @@ export const SetOBSSourceTextEffectType: EffectType<{
             <input type="checkbox" ng-click="toggleSource()" ng-checked="effect.textSource === 'file'"  aria-label="..." >
             <div class="control__indicator"></div>
         </label>
-        <firebot-input ng-if="effect.textSource === 'static'" model="effect.text"></firebot-input>
+        <firebot-input ng-if="effect.textSource === 'static'" model="effect.text" use-text-area="true"></firebot-input>
         <file-chooser ng-if="effect.textSource === 'file'" model="effect.file" options="{ filters: [ {name: 'Text File', extensions: ['txt']}, {name: 'All Files', extensions: ['*']} ]}" on-update="textFileUpdated(filepath)"></file-chooser>
     </eos-container>
   `,
