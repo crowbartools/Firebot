c41dfc3fb57fe65db6e18e240b306b347ac6e238
Zack Williamson <zwilliamson@outlook.com>
2023-02-04 15:51:17 -0500

    chore: new followers OAuth scope

--- src/backend/auth/twitch-auth.js
+++ src/backend/auth/twitch-auth.js
@@ -76,6 +76,7 @@ const STREAMER_ACCOUNT_PROVIDER = {
         'moderator:read:blocked_terms',
         'moderator:read:chat_settings',
         'moderator:read:chatters',
+        'moderator:read:followers',
         'moderator:read:shield_mode',
         'moderator:read:shoutouts',
         'user:edit:broadcast',
