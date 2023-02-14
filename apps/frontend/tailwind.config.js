32ee592855acc272dbed393f154dbda86b891015
SReject <SReject@users.noreply.github.com>
2023-02-14 04:34:56 -0500

    chore: add tailwind

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+//eslint-disable-next-line @typescript-eslint/no-var-requires
+// const colors = require("tailwindcss/colors");
+
+module.exports = {
+    content: [
+        `src/**/*.{js,ts,jsx,tsx}`
+    ],
+    theme: {
+        extend: {
+            colors: {
+            },
+        },
+    },
+    plugins: [],
+};
