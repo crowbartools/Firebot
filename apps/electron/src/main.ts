bb6f231123cbb47a4a49facf2a40c2a7485c5e97
SReject <SReject@users.noreply.github.com>
2023-02-15 09:47:50 -0500

    dev: add basic electron logic

--- apps/electron/src/main.ts
+++ apps/electron/src/main.ts
@@ -1,1 +1,44 @@
-export {};
+import { app, BrowserWindow} from 'electron';
+import path from 'node:path';
+
+let mainWindow : BrowserWindow;
+const createWindow = () => {
+    if (mainWindow) {
+        return;
+    }
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    const loadFile = path.join(__dirname, "../src/sample.html");
+    mainWindow.loadFile(loadFile);
+}
+
+
+process.on('uncaughtException', function (err) {
+    console.error(err.stack);
+    console.log("Node NOT Exiting...");
+    app.quit();
+});
+
+(async () => {
+
+    await app.whenReady();
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+
+
+
+    createWindow();
+})();
