3832f8aca2ee92c23123f78177c71ba09a766ac0
Erik Bigler <erikbigler@gmail.com>
2023-02-16 23:26:04 -0600

    start to build out theming support

--- config/tailwind-config/index.js
+++ config/tailwind-config/index.js
@@ -1,5 +1,24 @@
 const path = require('node:path');
 const root = path.resolve(__dirname, '../../');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const FIREBOT_BRAND_COLORS = {
+    maximumBlueGreen: '#4BBABC',
+    fireOpal: '#D63837',
+    pear: '#D2D50D',
+    sunglow: '#FFBE00',
+    aero: '#5FA1D9',
+    honoluluBlue: '#005B9F',
+    richBlack: '#071018',
+}
+
+// slab: {
+//     900: "#0A0A0C",
+//     700: "#18181B",
+//     600: "#27272A"
+// }
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
       // absolute path to ui component library
@@ -9,16 +28,48 @@ module.exports = {
         './src/pages/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}',
     ],
-    theme: {
-        extend: {
-            colors: {
-                slab: {
-                    900: "#0A0A0C",
-                    700: "#18181B",
-                    600: "#27272A"
+    // theme: {
+    //     extend: {
+    //         fontFamily: {
+    //             sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+    //         },
+    //     },
+    // },
+    safelist: [
+        'snow',
+        'slate',
+    ],
+    plugins: [
+        require('tailwindcss-themer')({
+            // "Shadow" theme
+            defaultTheme: {
+                extend: {
+                    colors: {
+                        "primary-bg": "#0A0A0C",
+                        "secondary-bg": "#18181B",
+                    },
                 }
-            }
-        },
-    },
-    plugins: [],
+            },
+            themes: [
+                {
+                    name: "slate",
+                    extend: {
+                        colors: {
+                            "primary-bg": "#27272A",
+                            "secondary-bg": "#36393F",
+                        }
+                    }
+                },
+                {
+                    name: "snow",
+                    extend: {
+                        colors: {
+                            "primary-bg": "#FFFFFF",
+                            "secondary-bg": "#D4D4D8",
+                        }
+                    }
+                },
+            ]
+        })
+    ],
 };
