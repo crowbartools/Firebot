b3e5cbcbb0c3e69f58264d67856b546e3cc60c58
SReject <SReject@users.noreply.github.com>
2023-02-19 11:51:23 -0500

    fix: nextjs export

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -3,4 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+if (process && process.env.NODE_ENV !== 'development') {
+  nextConfig.images = { unoptimized: true }
+}
+
 module.exports = nextConfig
