5e25bfa55753984803c96da76735eee14fdba1e4
Zack Williamson <zwilliamson@outlook.com>
2023-03-04 09:58:24 -0500

    chore: update compile output names

--- grunt/compile.js
+++ grunt/compile.js
@@ -11,6 +11,7 @@ module.exports = function (grunt) {
     const macPathOut = path.resolve(__dirname, '../dist/install/darwin');
     const macDmgIcon = path.resolve(__dirname, `../build/gui/images/logo_transparent_2.png`);
     const macDmgBg = path.resolve(__dirname, `../build/gui/images/firebot_dmg_bg.png`);
+    const version = grunt.config('pkg').version;
 
     grunt.config.merge({
         'create-windows-installer': {
@@ -22,14 +23,16 @@ module.exports = function (grunt) {
                 setupIcon: path.join(__dirname, '../build/gui/images/icon_transparent.ico'),
                 exe: "Firebot v5.exe",
                 title: "Firebot v5",
-                setupExe: "FirebotV5Setup.exe",
+                name: "firebot",
+                version: version,
+                setupExe: `firebot-v${version}-setup.exe`,
                 noMsi: true
             }
         },
         compress: {
             linux: {
                 options: {
-                    archive: path.join(__dirname, '../dist/install/Linux/Firebot-linux-x64.tar.gz'),
+                    archive: path.join(__dirname, `../dist/install/Linux/firebot-v${version}-linux-x64.tar.gz`),
                     mode: 'tgz'
                 },
                 files: [{
@@ -42,7 +45,7 @@ module.exports = function (grunt) {
         },
         shell: {
             'compile-darwin': {
-                command: `npx --no-install electron-installer-dmg "${macPathIn}" Firebot --out="${macPathOut}" --background="${macDmgBg}" --icon="${macDmgIcon}" --title="Firebot Installer" --debug`
+                command: `npx --no-install electron-installer-dmg "${macPathIn}" firebot-v${version}-macos-x64 --out="${macPathOut}" --background="${macDmgBg}" --icon="${macDmgIcon}" --title="Firebot Installer" --debug`
             }
         }
     });
