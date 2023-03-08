f49a2c80e4c0926a2c0cd7bda305508085df92dd
SReject <SReject@users.noreply.github.com>
2023-03-08 12:58:50 -0500

    feat: begin populating firebotAppDetails

--- src/backend/app-management/electron/preload.js
+++ src/backend/app-management/electron/preload.js
@@ -1,9 +1,16 @@
 'use strict';
-const {
-    app,
-    contextBridge
-} = require('electron');
+
+const os = require('node:os');
+const { app, contextBridge } = require('electron');
+
+
 
 contextBridge.exposeInMainWorld('firebotAppDetails', {
-
+    version: app.getVersion(),
+    packaged: app.isPackaged,
+    os: {
+        isWindows: process.platform === 'win32',
+        type: os.type(),
+        release: os.release()
+    }
 });
