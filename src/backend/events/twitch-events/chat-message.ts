067ba9463ac3df8e3588ece11e2f9f7bacbc6b29
Dennis Rijsdijk <git@dennis.gg>
2023-11-12 14:06:33 +0100

    add metadata to chat message event

--- src/backend/events/twitch-events/chat-message.ts
+++ src/backend/events/twitch-events/chat-message.ts
@@ -3,6 +3,7 @@ import eventManager from "../../events/EventManager";
 
 export function triggerChatMessage(firebotChatMessage: FirebotChatMessage): void {
     eventManager.triggerEvent("twitch", "chat-message", {
+        userId: firebotChatMessage.userId,
         userIdName: firebotChatMessage.useridname,
         username: firebotChatMessage.username,
         twitchUserRoles: firebotChatMessage.roles,
