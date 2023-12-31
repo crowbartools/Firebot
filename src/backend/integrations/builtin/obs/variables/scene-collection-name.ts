4539e519dc759a846028b0e642af837f39a0a59b
Zack Williamson <zwilliamson@outlook.com>
2023-12-31 12:23:48 -0500

    fix(obs): trigger param didn't save the first time

--- src/backend/integrations/builtin/obs/variables/scene-collection-name.ts
+++ src/backend/integrations/builtin/obs/variables/scene-collection-name.ts
@@ -8,7 +8,7 @@ export const SceneCollectionNameVariable: ReplaceVariable = {
       "The name of the OBS scene collection that triggered the event, or the name of the current OBS scene collection if there is no event. If OBS isn't running, it returns 'Unknown'.",
         possibleDataOutput: ["text"]
     },
-    evaluator: async () => {
+    evaluator: async (trigger) => {
         const currentSceneCollectionName = trigger.metadata?.eventData?.sceneCollectionName ?? await getCurrentSceneCollectionName();
         return currentSceneCollectionName ?? "Unknown";
     }
