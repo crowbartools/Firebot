99bbd9c9e42dbbc49f81b4de990d26fc7f526691
Erik Bigler <erikbigler@gmail.com>
2024-01-05 13:50:27 -0600

    fix: app menu icon path failing to resolve blocking firebot from opening on macos

--- src/backend/app-management/electron/window-management.js
+++ src/backend/app-management/electron/window-management.js
@@ -90,12 +90,17 @@ function createStreamPreviewWindow() {
 }
 
 async function createIconImage(relativeIconPath) {
-    const iconPath = path.resolve(__dirname, relativeIconPath);
+    const iconPath = path.join(__dirname, relativeIconPath);
     if (process.platform === "darwin") {
-        return nativeImage.createThumbnailFromPath(iconPath, {
-            width: 14,
-            height: 14
-        });
+        try {
+            return await nativeImage.createThumbnailFromPath(iconPath, {
+                width: 14,
+                height: 14
+            });
+        } catch (e) {
+            logger.error(`Failed to create icon image for path: ${relativeIconPath}`, relativeIconPath, e);
+            return;
+        }
     }
     return iconPath;
 }
