8ecd6825628946ff52c4d982763d9269f29f7eb0
CKY- <xx_cky_xx@hotmail.com>
2024-01-23 17:34:54 -0700

    Fix: pronouns to return fallbacks correctly

--- src/backend/variables/builtin/user/pronouns.ts
+++ src/backend/variables/builtin/user/pronouns.ts
@@ -45,41 +45,58 @@ const PronounVariable: ReplaceVariable = {
         categories: [VariableCategory.COMMON],
         possibleDataOutput: [OutputDataType.TEXT]
     },
-    evaluator: async (trigger, username: string, pronounNumber: number, fallback: string) => {
+    evaluator: async (trigger,
+        username: string,
+        pronounNumber: number | string = 0,
+        // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+        fallback : string = "they/them") => {
         try {
+            if (typeof pronounNumber === 'string' || <unknown>pronounNumber instanceof String) {
+                pronounNumber = Number(`${pronounNumber}`);
+            }
+
+            if (!Number.isFinite(<number>pronounNumber)) {
+                logger.warn("Pronoun index not a number using they/them");
+                return fallback;
+            }
+
             const pronouns = (await callUrl('https://pronouns.alejo.io/api/pronouns')).data;
-            const userPronounData = (await callUrl(`https://pronouns.alejo.io/api/users/${username}`)).data[0];
+            let userPronounData = (await callUrl(`https://pronouns.alejo.io/api/users/${username}`)).data[0];
 
-            if (pronouns != null && userPronounData != null) {
-                try {
-                    const pronoun = pronouns.find(p => p.name === userPronounData.pronoun_id);
-                    if (pronoun != null) {
-                        if (pronounNumber === 0) {
-                            return pronoun.display;
-                        }
-
-                        const pronounArray = pronoun.display.split('/');
-
-                        if (pronounArray.length === 1) {
-                            return pronounArray[0];
-                        }
-
-                        if (pronounArray.length >= pronounNumber) {
-                            return pronounArray[pronounNumber - 1];
-                        }
-                        return fallback || "them/they";
-                    }
-
-                    return fallback;
-                } catch (err) {
-                    logger.warn("error when parsing pronoun api", err);
-                    return fallback || "they/them";
+            try {
+                let pronounArray = [];
+                if (userPronounData == null || userPronounData === undefined) {
+                    userPronounData = { "pronoun_id": "theythem" };
                 }
+
+                const pronoun = pronouns.find(p => p.name === userPronounData.pronoun_id);
+                if (pronoun != null) {
+                    pronounArray = pronoun.display.split('/');
+                } else {
+                    pronounArray = fallback.split('/');
+                }
+
+                if (pronounNumber === 0) {
+                    return pronoun.display;
+                }
+
+                if (pronounArray.length === 1) {
+                    return pronounArray[0];
+                }
+
+                if (pronounArray.length >= pronounNumber) {
+                    return pronounArray[pronounNumber - 1];
+                }
+                return fallback;
+
+            } catch (err) {
+                logger.warn("error when parsing pronoun api", err);
+                return fallback;
             }
 
         } catch (err) {
             logger.warn("error when parsing pronoun api", err);
-            return fallback || "they/them";
+            return fallback;
         }
     }
 };
