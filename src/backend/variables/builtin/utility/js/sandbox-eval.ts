524974e8ff42960a3ee776adfd6390009feaf8a8
SReject <SReject@users.noreply.github.com>
2024-02-07 15:23:51 -0500

    feat: add examples for $evalJs

--- src/backend/variables/builtin/utility/js/sandbox-eval.ts
+++ src/backend/variables/builtin/utility/js/sandbox-eval.ts
@@ -26,7 +26,21 @@ const model : ReplaceVariable = {
     definition: {
         handle: "evalJs",
         usage: "evalJs[`` code ``, ...parameters]",
-        description: 'Evaluates the given js in a sandboxed browser instance.<br/><br/>Parameters can be accessed via parameters[N] within the js.<br/><br/>You must use return to return a result from the evaluation.',
+        description: 'Evaluates the given js in a sandboxed browser instance.<br/><br/>Parameters can be accessed via parameters[N] within the js.<br/>Event metadata can be accessed via metadata.*<br/><br/>You must use return to return a result from the evaluation.',
+        examples: [
+            {
+                usage: 'evalJs[``return parameters[0]``, test]',
+                description: 'Returns the first parameter passed to $evalJS: "test"'
+            },
+            {
+                usage: 'evalJs[``return metadata.username``]',
+                description: 'Returns the username from the event\'s metadata'
+            },
+            {
+                usage: 'evalJs[``return await Firebot.sum[1,2,3,4]``]',
+                description: 'Calls the sum firebot api and returns the result'
+            }
+        ],
         categories: [VariableCategory.ADVANCED],
         possibleDataOutput: [OutputDataType.ALL]
     },
