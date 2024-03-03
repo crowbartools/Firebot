981a6a591b9ef349bb70d346f04a50f8cab5e52b
Chuck Kostalnick <heyaapl@users.noreply.github.com>
2024-03-03 10:36:58 -0700

    Add New Chat Effect Option

--- src/backend/effects/builtin/chat.js
+++ src/backend/effects/builtin/chat.js
@@ -19,6 +19,10 @@ const effect = {
         <textarea ng-model="effect.message" class="form-control" name="text" placeholder="Enter message" rows="4" cols="40" replace-variables></textarea>
         <div style="color: #fb7373;" ng-if="effect.message && effect.message.length > 500">Chat messages cannot be longer than 500 characters. This message will get automatically chunked into multiple messages if it's too long after all replace variables have been populated.</div>
         <div style="display: flex; flex-direction: row; width: 100%; height: 36px; margin: 10px 0 10px; align-items: center;">
+            <label class="control-fb control--checkbox" style="margin: 0px 15px 0px 0px"> Use "/me"<tooltip text="'Applies Italics to your Chat Message or your Chat Color if used in a Whisper'"></tooltip>
+                <input type="checkbox" ng-model="effect.me">
+                <div class="control__indicator"></div>
+            </label>
             <label class="control-fb control--checkbox" style="margin: 0px 15px 0px 0px"> Whisper
                 <input type="checkbox" ng-init="whisper = (effect.whisper != null && effect.whisper !== '')" ng-model="whisper" ng-click="effect.whisper = ''">
                 <div class="control__indicator"></div>
@@ -55,7 +59,10 @@ const effect = {
         } else if (trigger.type === EffectTrigger.EVENT) {
             messageId = trigger.metadata.eventData?.chatMessage?.id;
         }
-
+        const meCommand = "/me ";
+        if (effect.me) {
+            effect.message = meCommand.concat(effect.message);
+        }
         await twitchChat.sendChatMessage(effect.message, effect.whisper, effect.chatter, !effect.whisper && effect.sendAsReply ? messageId : undefined);
 
         return true;
