e4083ecc9a17691b32e8b2f6999ab741432bfd22
Zack Williamson <zwilliamson@outlook.com>
2024-03-03 17:03:03 -0500

    feat(vars): add rawRandomCustomRoleUser

--- src/backend/variables/builtin/user/roles/index.ts
+++ src/backend/variables/builtin/user/roles/index.ts
@@ -4,6 +4,7 @@ import customRoleUsersRaw from './custom-role-users-raw';
 import hasRole from './has-role';
 import hasRoles from './has-roles';
 import randomCustomRoleUser from './random-custom-role-user';
+import randomCustomRoleUserRaw from './random-custom-role-user-raw';
 import userRoles from './user-roles';
 import userRolesRaw from './user-roles-raw';
 
@@ -14,6 +15,7 @@ export default [
     hasRole,
     hasRoles,
     randomCustomRoleUser,
+    randomCustomRoleUserRaw,
     userRoles,
     userRolesRaw
 ];
