fef1ff3cef5184a3cbce97df556ea41f80c1cc24
Chuck Kostalnick <heyaapl@gmail.com>
2024-03-08 17:47:59 -0700

    feat: /me Chat Effect Option (#2440)
    * Add New Chat Effect Option
    
    * Refactored Chatjs
    
    * Refactored Chat Effect Input
    
    * Implement Additional Feedback
    
    ---------
    
    Co-authored-by: Zack Williamson <zwilliamson@outlook.com>
    Co-authored-by: SReject <SReject@users.noreply.github.com>
    Co-authored-by: Chuck Kostalnick <heyaapl@users.noreply.github.com>
    Co-authored-by: Erik Bigler <erikbigler@gmail.com>

--- src/backend/effects/builtin/chat.js
+++ src/backend/effects/builtin/chat.js
@@ -16,31 +16,50 @@ const effect = {
     <eos-chatter-select effect="effect" title="Chat as"></eos-chatter-select>
 
     <eos-container header="Message To Send" pad-top="true">
-        <textarea ng-model="effect.message" class="form-control" name="text" placeholder="Enter message" rows="4" cols="40" replace-variables></textarea>
+        <firebot-input 
+            model="effect.message" 
+            use-text-area="true"
+            placeholder-text="Enter message"
+            rows="4"
+            cols="40"
+        />
         <div style="color: #fb7373;" ng-if="effect.message && effect.message.length > 500">Chat messages cannot be longer than 500 characters. This message will get automatically chunked into multiple messages if it's too long after all replace variables have been populated.</div>
         <div style="display: flex; flex-direction: row; width: 100%; height: 36px; margin: 10px 0 10px; align-items: center;">
-            <label class="control-fb control--checkbox" style="margin: 0px 15px 0px 0px"> Whisper
-                <input type="checkbox" ng-init="whisper = (effect.whisper != null && effect.whisper !== '')" ng-model="whisper" ng-click="effect.whisper = ''">
-                <div class="control__indicator"></div>
-            </label>
-            <div ng-show="whisper">
-                <div class="input-group">
-                    <span class="input-group-addon" id="chat-whisper-effect-type">To</span>
-                    <input ng-model="effect.whisper" type="text" class="form-control" id="chat-whisper-setting" aria-describedby="chat-text-effect-type" placeholder="Username" replace-variables>
-                </div>
+            <firebot-checkbox 
+                label="Use '/me'" 
+                tooltip="Applies Italics to your Chat Message or your Chat Color if used in a Whisper" 
+                model="effect.me"
+                style="margin: 0px 15px 0px 0px"
+            />
+            <firebot-checkbox 
+                label="Whisper"
+                model="showWhisperInput"
+                style="margin: 0px 15px 0px 0px"
+                ng-click="effect.whisper = ''"
+            />
+            <div ng-show="showWhisperInput">
+                <firebot-input 
+                    input-title="To"
+                    model="effect.whisper" 
+                    placeholder-text="Username"
+                />
             </div>
         </div>
-        <p ng-show="whisper" class="muted" style="font-size:11px;"><b>ProTip:</b> To whisper the associated user, put <b>$user</b> in the whisper field.</p>
-        <div ng-hide="whisper">
-            <label class="control-fb control--checkbox" style="margin: 0px 15px 0px 0px"> Send as reply<tooltip text="'Replying only works within a Command or Chat Message event'"></tooltip>
-                <input type="checkbox" ng-model="effect.sendAsReply">
-                <div class="control__indicator"></div>
-            </label>
+        <p ng-show="effect.whisper" class="muted" style="font-size:11px;"><b>ProTip:</b> To whisper the associated user, put <b>$user</b> in the whisper field.</p>
+        <div ng-hide="effect.whisper">
+            <firebot-checkbox 
+                label="Send as reply" 
+                tooltip="Replying only works within a Command or Chat Message event" 
+                model="effect.sendAsReply"
+                style="margin: 0px 15px 0px 0px"
+            />
         </div>
     </eos-container>
 
     `,
-    optionsController: () => {},
+    optionsController: ($scope) => {
+        $scope.showWhisperInput = $scope.effect.whisper != null && $scope.effect.whisper !== ''
+    },
     optionsValidator: effect => {
         const errors = [];
         if (effect.message == null || effect.message === "") {
@@ -54,6 +73,10 @@ const effect = {
             messageId = trigger.metadata.chatMessage.id;
         } else if (trigger.type === EffectTrigger.EVENT) {
             messageId = trigger.metadata.eventData?.chatMessage?.id;
+        }
+
+        if (effect.me) {
+            effect.message = `/me ${effect.message}`;
         }
 
         await twitchChat.sendChatMessage(effect.message, effect.whisper, effect.chatter, !effect.whisper && effect.sendAsReply ? messageId : undefined);
