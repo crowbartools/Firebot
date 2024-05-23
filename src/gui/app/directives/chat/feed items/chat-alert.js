691182b69e2891096c6ebe3b30f4f079c7a935b5
phroggster <phroggster@users.noreply.github.com>
2024-05-23 03:38:19 -0500

    Feat: add formatting to chatAlert effects

--- src/gui/app/directives/chat/feed items/chat-alert.js
+++ src/gui/app/directives/chat/feed items/chat-alert.js
@@ -1,5 +1,9 @@
 "use strict";
-(function() {
+
+(function () {
+    const marked = require("marked");
+    const { sanitize } = require("dompurify");
+
     angular
         .module('firebotApp')
         .component("chatAlert", {
@@ -9,8 +13,24 @@
             template: `
                 <div class="chat-alert">
                     <span style="font-size:25px;margin-right: 10px;"><i class="fad fa-exclamation-circle"></i></span>
-                    <span style="margin-top: 8px;">{{$ctrl.message}}</span>
+                    <span style="margin-top: 8px;" ng-bind-html="$ctrl.message"></span>
                 </div>
-            `
+            `,
+            controller: function ($scope, $sce) {
+                const $ctrl = this;
+                $ctrl.message = null;
+
+                const parseMarkdown = (text) => {
+                    return $sce.trustAsHtml(
+                        sanitize(marked(text))
+                    );
+                };
+
+                $scope.$parent.$watch("trigger", function () {
+                    if ($ctrl.message) {
+                        $ctrl.message = parseMarkdown($ctrl.message);
+                    }
+                });
+            }
         });
 }());
