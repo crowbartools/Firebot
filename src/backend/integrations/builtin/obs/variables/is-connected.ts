f00a7825dd00c1bff3309926ed42008870904719
Izzy Deane <github@oceanity.net>
2024-07-14 20:45:08 -0400

    Implemented `obsIsConnected` Replace Variable

--- src/backend/integrations/builtin/obs/variables/is-connected.ts
+++ src/backend/integrations/builtin/obs/variables/is-connected.ts
@@ -1,15 +1,15 @@
 import { ReplaceVariable } from "../../../../../types/variables";
-import { isRecording } from "../obs-remote";
+import { isConnected } from "../obs-remote";
 
 export const IsConnectedVariable: ReplaceVariable = {
     definition: {
         handle: "obsIsConnected",
         description:
-      "Returns 'true' if OBS is currently recording or 'false' if it is not.",
+      "Returns 'true' if OBS is currently connected or 'false' if it is not.",
         possibleDataOutput: ["text"]
     },
     evaluator: async () => {
-        const recordState = await isRecording();
-        return recordState ?? false;
+        const connectionState = isConnected();
+        return connectionState ?? false;
     }
 };
