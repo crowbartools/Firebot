a4451866bbc4a6dcb3790ddf41a32976afec2320
Izzy Deane <github@oceanity.net>
2024-07-16 18:43:25 -0400

    Updated description to include both terms

--- src/backend/variables/builtin/user/user-bio.ts
+++ src/backend/variables/builtin/user/user-bio.ts
@@ -7,15 +7,15 @@ const model : ReplaceVariable = {
     definition: {
         handle: "userBio",
         usage: "userBio",
-        description: "Gets the description of the associated user (ie who triggered command, pressed button, etc).",
+        description: "Gets the bio/description of the associated user (ie who triggered command, pressed button, etc).",
         examples: [
             {
                 usage: "userBio[$target]",
-                description: "When in a command, gets the the description of the target user."
+                description: "When in a command, gets the the bio/description of the target user."
             },
             {
                 usage: "userBio[ebiggz]",
-                description: "Gets the description of a specific user."
+                description: "Gets the bio/description of a specific user."
             }
         ],
         categories: [VariableCategory.USER],
