e36213920c9c1b4dc730b9b674b72356a295aa19
Izzy Deane <github@oceanity.net>
2024-07-16 17:40:09 -0400

    Moved description to bio

--- src/backend/variables/builtin/user/user-description.ts
+++
@@ -1,6 +0,0 @@
-import { ReplaceVariable } from "../../../../types/variables";
-import baseUserDescriptionVar from "./base-user-description";
-
-const model : ReplaceVariable = baseUserDescriptionVar("userDescription");
-
-export default model;
