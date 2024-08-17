824ad2d526f3353c9617d1d7c8c5055aacfc4429
Erik Bigler <erikbigler@gmail.com>
2024-08-17 14:57:53 -0500

    wip

--- grunt/typescript.js
+++ grunt/typescript.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     grunt.config.merge({
         shell: {
             tsc: {
-                command: 'tsc'
+                command: 'tsc --build'
             }
         }
     });
