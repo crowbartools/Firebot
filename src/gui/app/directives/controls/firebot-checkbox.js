094960653cbcb86c930cf9202674701c68c53dbd
Erik Bigler <erikbigler@gmail.com>
2024-08-18 04:58:49 -0500

    chore: add onChange event callback to firebot-checkbox

--- src/gui/app/directives/controls/firebot-checkbox.js
+++ src/gui/app/directives/controls/firebot-checkbox.js
@@ -10,7 +10,8 @@
                 tooltipPlacement: "@?",
                 model: "=",
                 style: "@?",
-                disabled: "<?"
+                disabled: "<?",
+                onChange: "&?"
             },
             template: `
             <label class="control-fb control--checkbox" style="{{$ctrl.style}}"> {{$ctrl.label}} <tooltip ng-if="$ctrl.tooltip" text="$ctrl.tooltip" placement="{{$ctrl.tooltipPlacement || ''}}"></tooltip>
@@ -18,6 +19,7 @@
                     type="checkbox"
                     ng-model="$ctrl.model"
                     ng-disabled="$ctrl.disabled"
+                    ng-change="$ctrl.onChange ? $ctrl.onChange({ newValue: $ctrl.model }) : undefined"
                 >
                 <div class="control__indicator"></div>
             </label>
