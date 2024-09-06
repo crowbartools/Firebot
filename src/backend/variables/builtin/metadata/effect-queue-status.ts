e7c4381d7c1c117829b7c2e31f2f6868c68c2091
Alf <2608282+alfw@users.noreply.github.com>
2024-09-06 15:28:26 +0200

    feat: added new events for queue status and new entry

--- src/backend/variables/builtin/metadata/effect-queue-status.ts
+++ src/backend/variables/builtin/metadata/effect-queue-status.ts
@@ -1,6 +1,7 @@
 import { ReplaceVariable } from "../../../../types/variables";
 import { EffectTrigger } from "../../../../shared/effect-constants";
 import { OutputDataType, VariableCategory } from "../../../../shared/variable-constants";
+import effectQueueManager from "../../../effects/queues/effect-queue-manager";
 
 const triggers = {};
 triggers[EffectTrigger.EVENT] = ["firebot:effect-queue-cleared", "firebot:effect-queue-added", "firebot:effect-queue-status"];
@@ -8,15 +9,17 @@ triggers[EffectTrigger.MANUAL] = true;
 
 const model: ReplaceVariable = {
     definition: {
-        handle: "effectQueueId",
-        description: "The ID of the effect queue.",
+        handle: "effectQueueStatus",
+        description: "The status of the effect queue.",
         triggers: triggers,
         categories: [VariableCategory.TRIGGER],
         possibleDataOutput: [OutputDataType.TEXT]
     },
     evaluator: (trigger) => {
         const queueId = trigger?.metadata?.eventData?.effectQueueId;
-        return queueId ?? "Unknown";
+        const effectQueue = effectQueueManager.getItem(queueId);
+
+        return effectQueue?.active ?? "Unknown";
     }
 };
 
