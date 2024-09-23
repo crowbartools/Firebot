c6295877285cbb470bf7c797efead524ddaac91c
Izzy Deane <github@oceanity.net>
2024-09-23 12:54:49 -0500

    Removed ext

--- src/backend/variables/builtin/utility/file-name.ts
+++ src/backend/variables/builtin/utility/file-name.ts
@@ -1,6 +1,6 @@
 import { ReplaceVariable, Trigger } from "../../../../types/variables";
 import { OutputDataType, VariableCategory } from "../../../../shared/variable-constants";
-import { basename } from "path";
+import { basename, extname } from "path";
 
 const model : ReplaceVariable = {
     definition: {
@@ -19,7 +19,7 @@ const model : ReplaceVariable = {
         }
 
         try {
-            return basename(filePath);
+            return basename(filePath, extname(filePath));
         } catch (err) {
             // Probably a directory or invalid filename
             return "[Invalid File Path]";
