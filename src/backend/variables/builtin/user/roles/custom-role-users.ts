8a5a636211a418e835d01f15f02cb0a94c836872
phroggster <phroggster@users.noreply.github.com>
2024-10-25 15:45:37 -0500

    Array, not object, not text: array
    - OutputDataType.TEXT really distracted me from the actual data type.
    - But it's entirely arrays, all the way down.

--- src/backend/variables/builtin/user/roles/custom-role-users.ts
+++ src/backend/variables/builtin/user/roles/custom-role-users.ts
@@ -8,7 +8,7 @@ const model : ReplaceVariable = {
         usage: "customRoleUsers[role]",
         description: "Returns an array of all the user display names in the specified custom role.",
         categories: [VariableCategory.USER],
-        possibleDataOutput: [OutputDataType.TEXT, OutputDataType.OBJECT],
+        possibleDataOutput: [OutputDataType.ARRAY],
         examples: [
             {
                 usage: "customRoleUsers[role, username]",
