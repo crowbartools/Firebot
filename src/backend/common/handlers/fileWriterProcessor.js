daab42b87577afe95afc0e819060779548929244
phroggster <phroggster@users.noreply.github.com>
2025-02-28 20:50:21 -0600

    More Flexible Replacement Token
    - I do have to question why anyone might be writing low ASCII control
      characters to a file, but they can do so freely now.

--- src/backend/common/handlers/fileWriterProcessor.js
+++ src/backend/common/handlers/fileWriterProcessor.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const logger = require("../../logwrapper");
+const uuid = require("uuid").v4;
 
 function doesTextExistInFile(filepath, text) {
     const contents = fs.readFileSync(filepath, { encoding: "utf8" });
@@ -66,9 +67,13 @@ exports.run = async (effect) => {
     }
 
     let text = effect.text || "";
-    text = text.replace(/\\\\n/g, "␚");
+    let escapedNewline = "␚";
+    while (text.includes(escapedNewline)) {
+        escapedNewline = `␚${uuid()}␚`;
+    }
+    text = text.replace(/\\\\n/g, escapedNewline);
     text = effect.writeMode === "suffix" ? text.replace(/\\n/g, "\n") : text.replace(/\\n/g, "\n").trim();
-    text = text.replace(/␚/g, "\\n");
+    text = text.replaceAll(escapedNewline, "\\n");
 
     try {
         if (effect.writeMode === "suffix") {
