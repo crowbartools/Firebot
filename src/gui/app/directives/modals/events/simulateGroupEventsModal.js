5b1c6d858fc0ac49aa614cf124bda1fd2e0bb748
Erik Bigler <erikbigler@gmail.com>
2025-03-10 01:49:26 -0600

    chore: remove force event to run checkbox

--- src/gui/app/directives/modals/events/simulateGroupEventsModal.js
+++ src/gui/app/directives/modals/events/simulateGroupEventsModal.js
@@ -28,13 +28,6 @@
                         ></command-option>
                     </div>
 
-                    <div ng-show="$ctrl.eventData.eventId != null">
-                        <hr class="divider" />
-                        <label class="control-fb control--checkbox"> Force event to run <tooltip text="'This will ensure that the simulated event will run, even if a similar event was recently triggered.'"></tooltip>
-                            <input type="checkbox" ng-model="$ctrl.eventData.forceRetrigger">
-                            <div class="control__indicator"></div>
-                        </label>
-                    </div>
                 </div>
                 <div class="modal-footer">
                     <button ng-if="$ctrl.hasPreviousProperties" type="button" class="btn btn-default pull-left" ng-click="$ctrl.loadPrevious()">Load Previous</button>
