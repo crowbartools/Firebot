0149e031b63fd79f357affecf8f87a7dffffb013
Dennis Rijsdijk <70665154+dennisrijsdijk@users.noreply.github.com>
2025-03-31 22:17:43 +0200

    feat: add array parameters to $min and $max (#2987) (#3063)

--- src/backend/variables/builtin/number/number-max.ts
+++ src/backend/variables/builtin/number/number-max.ts
@@ -1,6 +1,11 @@
 import { ReplaceVariable, Trigger } from "../../../../types/variables";
 import { OutputDataType, VariableCategory } from "../../../../shared/variable-constants";
 
+const normalizeNumber = (input) => {
+    const value = Number(input);
+    return Number.isInteger(value) ? value : null;
+};
+
 const model : ReplaceVariable = {
     definition: {
         handle: "max",
@@ -10,6 +15,18 @@ const model : ReplaceVariable = {
             {
                 usage: "max[1, 5, 3, 10]",
                 description: "Returns 10, the highest value from the input numbers"
+            },
+            {
+                usage: "max[numberArray]",
+                description: "Returns the highest value from the input array of numbers"
+            },
+            {
+                usage: "max[``[1, 5, 3, 10]``]",
+                description: "Returns 10, the highest value from the input stringified array of numbers"
+            },
+            {
+                usage: "max[``[8, 12]``, 5, 3, 10]",
+                description: "Returns 12, the highest value from the input stringified array and numbers"
             }
         ],
         categories: [VariableCategory.NUMBERS],
@@ -17,14 +34,23 @@ const model : ReplaceVariable = {
     },
     evaluator: (
         trigger: Trigger,
-        ...args: Array<string | number>
+        ...args: Array<string | number | Array<string | number>>
     ) : number => {
-        const numArgs : number[] = args.map((value) => {
-            value = Number(value);
-            if (Number.isInteger(value)) {
-                return value;
+        const numArgs : number[] = args.flatMap((value) => {
+            if (Array.isArray(value)) {
+                return value.map(normalizeNumber);
             }
-            return null;
+            if (typeof value === "string") {
+                try {
+                    const parsed = JSON.parse(value);
+                    if (Array.isArray(parsed)) {
+                        return parsed.map(normalizeNumber);
+                    }
+                } catch (e) {
+                    return normalizeNumber(value);
+                }
+            }
+            return normalizeNumber(value);
         }).filter(value => value !== null);
 
         const max = Math.max(...numArgs);
--- src/backend/variables/builtin/number/number-min.ts
+++ src/backend/variables/builtin/number/number-min.ts
@@ -1,6 +1,11 @@
 import { ReplaceVariable, Trigger } from "../../../../types/variables";
 import { OutputDataType, VariableCategory } from "../../../../shared/variable-constants";
 
+const normalizeNumber = (input) => {
+    const value = Number(input);
+    return Number.isInteger(value) ? value : null;
+};
+
 const model : ReplaceVariable = {
     definition: {
         handle: "min",
@@ -10,6 +15,18 @@ const model : ReplaceVariable = {
             {
                 usage: "min[1, 5, 3, 10]",
                 description: "Returns 1, the lowest value from the input numbers"
+            },
+            {
+                usage: "min[numberArray]",
+                description: "Returns the lowest value from the input array of numbers"
+            },
+            {
+                usage: "min[``[1, 5, 3, 10]``]",
+                description: "Returns 1, the lowest value from the input stringified array of numbers"
+            },
+            {
+                usage: "min[``[8, 12]``, 5, 3, 10]",
+                description: "Returns 3, the lowest value from the input stringified array and numbers"
             }
         ],
         categories: [VariableCategory.NUMBERS],
@@ -19,12 +36,21 @@ const model : ReplaceVariable = {
         trigger: Trigger,
         ...args: Array<string | number>
     ) : number => {
-        const numArgs : number[] = args.map((value) => {
-            value = Number(value);
-            if (Number.isInteger(value)) {
-                return value;
+        const numArgs : number[] = args.flatMap((value) => {
+            if (Array.isArray(value)) {
+                return value.map(normalizeNumber);
             }
-            return null;
+            if (typeof value === "string") {
+                try {
+                    const parsed = JSON.parse(value);
+                    if (Array.isArray(parsed)) {
+                        return parsed.map(normalizeNumber);
+                    }
+                } catch (e) {
+                    return normalizeNumber(value);
+                }
+            }
+            return normalizeNumber(value);
         }).filter(value => value !== null);
 
         const max = Math.min(...numArgs);
