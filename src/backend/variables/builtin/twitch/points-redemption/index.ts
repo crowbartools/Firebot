efc560452bb0ef6e7bead0aa53e587b8f7ccc322
The Static Mage <199814801+TheStaticMage@users.noreply.github.com>
2025-09-12 00:00:16 -0500

    Remove redemptionMessage (just use rewardMessage)

--- src/backend/variables/builtin/twitch/points-redemption/index.ts
+++ src/backend/variables/builtin/twitch/points-redemption/index.ts
@@ -1,9 +1,7 @@
-import redemptionMessage from "./redemption-message";
 import unlockedEmoteName from "./unlocked-emote-name";
 import unlockedEmoteUrl from "./unlocked-emote-url";
 
 export default [
-    redemptionMessage,
     unlockedEmoteName,
     unlockedEmoteUrl
 ];
