3d76c078207037b3a70eb870da5446327bedd60f
Oceanity <github@oceanity.net>
2025-09-22 08:42:11 -0700

    feat(variables): :sparkles: Added filters and variables for if channel is live (#3241)
    Introduced a new `$isChannelLive`/`$isChannelLive[username]` variable and added filters to Commands and Events to only trigger when the channel is live

--- src/backend/events/filters/builtin/twitch/index.ts
+++ src/backend/events/filters/builtin/twitch/index.ts
@@ -1,7 +1,8 @@
 import bitsBadgeTier from "./bits-badge-tier";
+import channelLive from "./channel-live";
+import chatMode from "./chat-mode";
 import chatModeDuration from "./chat-mode-duration";
 import chatModeSetting from "./chat-mode-setting";
-import chatMode from "./chat-mode";
 import cheerBitsAmount from "./cheer-bits-amount";
 import giftCount from "./gift-count";
 import giftDuration from "./gift-duration";
@@ -10,8 +11,8 @@ import isAnonymous from "./is-anonymous";
 import lifetimeGiftCount from "./lifetime-gift-count";
 import message from "./message";
 import raidViewerCount from "./raid-viewer-count";
-import rewardName from "./reward-name";
 import reward from "./reward";
+import rewardName from "./reward-name";
 import sharedChat from "./shared-chat";
 import sharedTrain from "./shared-train";
 import streamCategory from "./stream-category";
@@ -22,6 +23,7 @@ import username from "./username";
 
 export default [
     bitsBadgeTier,
+    channelLive,
     chatModeDuration,
     chatModeSetting,
     chatMode,
