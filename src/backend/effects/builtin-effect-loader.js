44629c510458293f7da8e6237cc2e858dc6f59b8
Zack Williamson <zwilliamson@outlook.com>
2025-10-02 22:16:49 -0400

    feat(effects): Show Toast Nofication

--- src/backend/effects/builtin-effect-loader.js
+++ src/backend/effects/builtin-effect-loader.js
@@ -53,6 +53,7 @@ exports.loadEffects = () => {
         'shoutout',
         'show-image', // No migration needed.
         'show-text',
+        'show-toast',
         'stop-effect-execution',
         'sync-profile-data',
         'take-screenshot',
