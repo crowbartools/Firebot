c70686173a7dca5d8e6bb2f0545d46b34e8ce5ea
Erik Bigler <erikbigler@gmail.com>
2025-10-06 23:08:05 -0600

    feat(overlay-widgets): "Text" widget
    #3283

--- src/backend/overlay-widgets/builtin-types/index.ts
+++ src/backend/overlay-widgets/builtin-types/index.ts
@@ -3,11 +3,13 @@ import { dynamicCountdown } from "./countdown/countdown-dynamic";
 import { custom} from "./custom/custom";
 import { customAdvanced } from "./custom/custom-advanced";
 import { counterDisplay } from "./counter-display/counter-display";
+import { text } from "./text/text";
 
 export default [
     progressbar,
     dynamicCountdown,
     counterDisplay,
     custom,
-    customAdvanced
+    customAdvanced,
+    text
 ];
