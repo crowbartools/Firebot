79d6777f00c4dd2b9301fb4702abf3cc2e2c6b40
Erik Bigler <erikbigler@gmail.com>
2025-10-07 21:57:35 -0600

    going with the darker one

--- src/gui/app/directives/settings/categories/general-settings.js
+++ src/gui/app/directives/settings/categories/general-settings.js
@@ -10,7 +10,7 @@
                     >
                         <firebot-select
                             aria-label="App Theme"
-                            options="{'Light': 'Light', 'Midnight': 'Midnight', 'PurpleSky': 'Purple Sky', 'PurpleSkySoft': 'Purple Sky (Soft)', 'Obsidian': 'Obsidian',}"
+                            options="{'Light': 'Light', 'Midnight': 'Midnight', 'PurpleSky': 'Purple Sky', 'Obsidian': 'Obsidian',}"
                             ng-init="selectedTheme = settings.getSetting('Theme')"
                             selected="selectedTheme"
                             on-update="settings.saveSetting('Theme', option)"
