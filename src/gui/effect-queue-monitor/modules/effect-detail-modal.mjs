e4ca6bac1d8ac0b8ba2a3728725674055a53bfa3
Zack Williamson <zwilliamson@outlook.com>
2025-10-19 03:31:50 -0400

    chore(linting): yelling about linebreaks

--- src/gui/effect-queue-monitor/modules/effect-detail-modal.mjs
+++ src/gui/effect-queue-monitor/modules/effect-detail-modal.mjs
@@ -43,7 +43,7 @@ const effectDetailModal = {
     },
     computed: {
         effectWithoutDefinition() {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
             const { __definition, ...rest } = this.effect;
             return rest;
         },
