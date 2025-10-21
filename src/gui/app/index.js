6595d980035e050094d46f66960fa4c2c1907635
Zack Williamson <zwilliamson@outlook.com>
2025-10-20 22:40:28 -0400

    chore: remove legacy frontend globals

--- src/gui/app/index.js
+++ src/gui/app/index.js
@@ -1,19 +1,5 @@
 "use strict";
-const electron = require("electron");
-const { ipcRenderer } = electron;
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-//from old Gobal.js
-const shell = require("electron").shell;
-
-/**
- * Don't remove the below imports for now, they are available globally to all frontend files
- * TODO: Refactor to import these modules in the files that actually use them instead
- * of making them available globally here
- */
-const fs = require("fs");
-const request = require("request");
-const path = require("path");
+const { ipcRenderer, shell } = require("electron");
 
 require("angular");
 require("angular-aria");
