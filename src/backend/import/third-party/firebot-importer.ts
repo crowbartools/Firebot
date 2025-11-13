baa53e769cbe4969f9e671509820fe766db55d56
CaveMobster <cavemobster@gmail.com>
2025-11-13 16:20:17 +0100

    Add a quote importer

---
+++ src/backend/import/third-party/firebot-importer.ts
@@ -0,0 +1,128 @@
+import fsp from "fs/promises";
+
+import type { ThirdPartyImporter } from "../../../types/import";
+import type { Quote } from "../../../types/quotes";
+
+import logger from "../../logwrapper";
+
+type ImportedFirebotViewer = {
+    id: number;
+    username: string;
+    twitchRoles: string;
+    lastSeen: string;
+    joinDate: string;
+    minutesInChannel: number;
+    chatMessages: number;
+};
+
+export const FirebotImporter: ThirdPartyImporter = {
+    id: "firebot",
+    appName: "Firebot",
+    filetypes: [
+        { name: "CSV File", extensions: ["csv"] }
+    ],
+    loadQuotes: async (filepath) => {
+        try {
+            const fileLines = (await fsp.readFile(filepath, { encoding: "utf8" }))
+                .split(/\r?\n/);
+
+            const headers = [
+                "ID",
+                "Text",
+                "Originator",
+                "Creator",
+                "Category",
+                "Created"
+            ];
+            // Validate header
+            const header = fileLines.shift();
+            if (header !== headers.join(",")) {
+                return {
+                    success: false,
+                    error: "Invalid file format"
+                };
+            }
+
+            const quotes: Quote[] = [];
+            fileLines.forEach((line) => {
+                // First we need the text of the quote, because the text can contain commas.
+                const splittedQuote = line.split('"');
+
+                const id = splittedQuote.shift().split(",")[0];
+                const metadata = splittedQuote.pop().split(",");
+                const text = splittedQuote.join("");
+
+                logger.debug(id);
+
+                // We're getting the comma off first, then get the rest.
+                // Game comes last, since a game/category can contain commas.
+                metadata.shift();
+                const originator = metadata.shift();
+                const creator = metadata.shift();
+                const createdAt = metadata.pop();
+                const game = metadata.join(",");
+
+                quotes.push({
+                    _id: Number(id),
+                    text: text,
+                    originator: originator,
+                    creator: creator,
+                    game: game,
+                    createdAt: createdAt
+                });
+            });
+
+            return {
+                success: true,
+                data: {
+                    quotes
+                }
+            };
+        } catch (error) {
+            logger.error(`Unexpected error while parsing Firebot quotes`, error);
+            return {
+                success: false,
+                error: (error as Error).message
+            };
+        }
+    },
+    loadViewers: async (filepath) => {
+        const fileLines = (await fsp.readFile(filepath, { encoding: "utf8" }))
+            .split(/\r?\n/);
+
+        const headers = [
+            "MixItUpID",
+            "TwitchID",
+            "TwitchUsername",
+            "YouTubeID",
+            "YouTubeUsername",
+            "TrovoID",
+            "TrovoUsername",
+            "Minutes",
+            "CustomTitle",
+            "TotalStreamsWatched",
+            "TotalAmountDonated",
+            "TotalSubsGifted",
+            "TotalSubsReceived",
+            "TotalChatMessagesSent",
+            "TotalTimesTagged",
+            "TotalCommandsRun",
+            "TotalMonthsSubbed",
+            "LastSeen"
+        ];
+
+        // Validate header
+        const header = fileLines.shift();
+        if (header !== headers.join("\t")) {
+            return {
+                success: false,
+                error: "Invalid file format"
+            };
+        }
+
+        return {
+            success: false,
+            error: "Not implemented"
+        };
+    }
+};
