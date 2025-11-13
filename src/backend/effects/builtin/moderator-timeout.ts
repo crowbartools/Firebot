5da124788278365fab0f2655f532bb1a69157f8e
Zack Williamson <zwilliamson@outlook.com>
2025-11-12 19:29:12 -0500

    feat(effects): add Ban/Timeout reason (#3316)

--- src/backend/effects/builtin/moderator-timeout.ts
+++ src/backend/effects/builtin/moderator-timeout.ts
@@ -5,6 +5,7 @@ import logger from '../../logwrapper';
 const effect: EffectType<{
     username: string;
     time: number;
+    reason: string;
 }> = {
     definition: {
         id: "firebot:modTimeout",
@@ -15,18 +16,27 @@ const effect: EffectType<{
         dependencies: ["chat"]
     },
     optionsTemplate: `
-    <eos-container header="Target" pad-top="true">
-        <div class="input-group">
-            <span class="input-group-addon" id="username-type">Username</span>
-            <input ng-model="effect.username" type="text" class="form-control" id="list-username-setting" aria-describedby="list-username-type" replace-variables menu-position="below">
-        </div>
-    </eos-container>
-    <eos-container header="Time" pad-top="true">
-        <div class="input-group">
-            <span class="input-group-addon" id="time-type">Time (Seconds)</span>
-            <input ng-model="effect.time" type="text" class="form-control" id="list-username-setting" aria-describedby="list-time-type" placeholder="Seconds" replace-variables="number">
-        </div>
-    </eos-container>
+        <eos-container header="Target" pad-top="true">
+            <div class="input-group">
+                <span class="input-group-addon" id="username-type">Username</span>
+                <input ng-model="effect.username" type="text" class="form-control" id="list-username-setting" aria-describedby="list-username-type" replace-variables menu-position="below">
+            </div>
+        </eos-container>
+
+        <eos-container header="Time" pad-top="true">
+            <div class="input-group">
+                <span class="input-group-addon" id="time-type">Time (Seconds)</span>
+                <input ng-model="effect.time" type="text" class="form-control" id="list-username-setting" aria-describedby="list-time-type" placeholder="Seconds" replace-variables="number">
+            </div>
+        </eos-container>
+
+        <eos-container header="Reason" pad-top="true">
+            <firebot-input
+                input-title="Reason"
+                placeholder-text="Timed out by Firebot"
+                model="effect.reason"
+            />
+        </eos-container>
     `,
     optionsValidator: (effect) => {
         const errors: string[] = [];
@@ -42,7 +52,8 @@ const effect: EffectType<{
         const user = await TwitchApi.users.getUserByName(effect.username);
 
         if (user != null) {
-            const result = await TwitchApi.moderation.timeoutUser(user.id, effect.time);
+            const reason = effect.reason?.length ? effect.reason : "Timed out by Firebot";
+            const result = await TwitchApi.moderation.timeoutUser(user.id, effect.time, reason);
 
             if (result === true) {
                 logger.debug(`${effect.username} was timed out for ${effect.time}s via the timeout effect.`);
