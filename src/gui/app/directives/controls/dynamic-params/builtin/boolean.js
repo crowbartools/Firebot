5ff6f238dbcfb566a870fafe1de84aa402c620b1
Zack Williamson <zwilliamson@outlook.com>
2025-11-16 23:54:34 -0500

    chore: add space between checkbox label and tooltip

--- src/gui/app/directives/controls/dynamic-params/builtin/boolean.js
+++ src/gui/app/directives/controls/dynamic-params/builtin/boolean.js
@@ -16,7 +16,7 @@
             <label class="control-fb control--checkbox" style="font-weight: 600;">
             <div class="flex items-center">
                 <span ng-if="$ctrl.title" ng-bind-html="$ctrl.title" class="markdown-container"></span>
-                <tooltip ng-if="$ctrl.description" text="$ctrl.description"></tooltip>
+                <tooltip ng-if="$ctrl.description" class="ml-2" text="$ctrl.description"></tooltip>
             </div>
               <input type="checkbox" ng-click="$ctrl.local = !$ctrl.local" ng-checked="$ctrl.local" aria-label="...">
               <div class="control__indicator"></div>
