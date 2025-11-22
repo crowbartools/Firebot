6241af8088aef4eefed89df30007c0049eff3aab
Erik Bigler <erikbigler@gmail.com>
2025-11-22 00:02:07 -0700

    feat(ui): overhaul ux for webhook and global values modals

--- src/gui/app/directives/modals/misc/edit-global-values-modal.js
+++ src/gui/app/directives/modals/misc/edit-global-values-modal.js
@@ -6,32 +6,44 @@
             template: `
                 <div class="modal-header">
                     <button type="button" class="close" aria-label="Close" ng-click="$ctrl.dismiss()"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Global Values</h4>
+                    <h4 class="modal-title">
+                        Global Values
+                    </h4>
                 </div>
-                <div class="modal-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr ng-repeat="value in $ctrl.globalValues track by value.name">
-                                <td>{{value.name}}</td>
-                                <td>
-                                    <div class="flex items-center justify-end" style="gap: 10px;">
-                                        <span class="clickable" ng-click="$ctrl.addOrEditGlobalValue(value)"><i class="far fa-edit"></i></span>
-                                        <span class="effect-delete-btn clickable" ng-click="$ctrl.removeGlobalValue(value)"><i class="far fa-trash-alt"></i></span>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <button class="btn btn-default btn-sm" ng-click="$ctrl.addOrEditGlobalValue()">+ Create Global Value</button>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" ng-click="$ctrl.dismiss()">Done</button>
+                <div class="modal-body firebot-list-container">
+                    <div class="firebot-list-description">
+                        <p class="muted" style="margin: 0 0 20px 0; font-size: 13px;">
+                            <i class="fas fa-info-circle" style="margin-right: 6px;"></i>
+                            Static values that are available throughout Firebot via a variable. Useful for reusing common values (e.g., API keys).
+                        </p>
+                    </div>
+
+                    <div class="firebot-list">
+                        <div ng-repeat="value in $ctrl.globalValues track by value.name" class="firebot-list-item">
+                            <div class="firebot-list-item-header">
+                                <div class="firebot-list-item-info">
+                                    <span class="firebot-list-item-name">{{value.name}}</span>
+                                </div>
+                                <div class="firebot-list-item-actions">
+                                    <button class="btn btn-default btn-sm"
+                                            ng-click="$ctrl.addOrEditGlobalValue(value)"
+                                            title="Edit global value">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                    <button class="btn btn-danger btn-sm"
+                                            ng-click="$ctrl.removeGlobalValue(value)"
+                                            title="Delete global value">
+                                        <i class="far fa-trash-alt"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-primary btn-sm" style="width: 100%;" ng-click="$ctrl.addOrEditGlobalValue()">
+                        <i class="fas fa-plus" style="margin-right: 6px;"></i>
+                        Create Global Value
+                    </button>
                 </div>
             `,
             bindings: {
