cf5d0b02d59f9ea6990761b1e0c614efa1ab8bcb
ebiggz <erikbigler@gmail.com>
2025-12-07 09:37:27 -0700

    fix: resume passing the webhook config to custom scripts

--- src/backend/common/handlers/custom-scripts/custom-script-helpers.js
+++ src/backend/common/handlers/custom-scripts/custom-script-helpers.js
@@ -24,8 +24,8 @@ class ScriptWebhookManager extends EventEmitter {
         this.scriptName = scriptName;
         this.setMaxListeners(0);
 
-        webhookManager.on("webhook-received", ({ config, ...data }) => {
-            if (config.scriptId !== this.scriptName) {
+        webhookManager.on("webhook-received", (data) => {
+            if (data.config.scriptId !== this.scriptName) {
                 return;
             }
             this.emit("webhook-received", data);
