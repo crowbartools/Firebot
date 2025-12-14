23615951db431cc1b881a6259e4695b13335d02b
Zack Williamson <zwilliamson@outlook.com>
2025-12-14 01:53:26 -0500

    fix: move all shutdown logic to willQuit

--- src/backend/app-management/electron/events/windows-all-closed.ts
+++ src/backend/app-management/electron/events/windows-all-closed.ts
@@ -1,36 +1,8 @@
+import { app } from "electron";
 import logger from "../../../logwrapper";
 
-export async function windowsAllClosed() {
-    const { app } = await import("electron");
-    const { BackupManager } = await import("../../../backup-manager");
-    const { CustomVariableManager } = await import("../../../common/custom-variable-manager");
-    const { HotkeyManager } = await import("../../../hotkeys/hotkey-manager");
-    const { ScheduledTaskManager } = await import("../../../timers/scheduled-task-manager");
-    const { SettingsManager } = await import("../../../common/settings-manager");
-    const customScriptRunner = await import("../../../common/handlers/custom-scripts/custom-script-runner");
-    const viewerOnlineStatusManager = (await import("../../../viewers/viewer-online-status-manager")).default;
-
+export function windowsAllClosed() {
     logger.debug("All windows closed triggered");
-
-    // Stop all scheduled tasks
-    ScheduledTaskManager.stop();
-
-    // Stop all custom scripts so they can clean up
-    await customScriptRunner.stopAllScripts();
-
-    // Unregister all shortcuts.
-    HotkeyManager.unregisterAllHotkeys();
-
-    // Persist custom variables
-    CustomVariableManager.persistVariablesToFile();
-
-    // Set all users to offline
-    await viewerOnlineStatusManager.setAllViewersOffline();
-
-    if (SettingsManager.getSetting("BackupOnExit")) {
-        // Make a backup
-        await BackupManager.startBackup(false);
-    }
 
     app.quit();
 };
